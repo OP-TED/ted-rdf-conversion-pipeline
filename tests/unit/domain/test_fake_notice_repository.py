1468ede433881ce98eb4109c614a288d79d218d4
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-02-10 18:09:45 +0200

    Create test_fake_notice_repository.py

---
+++ tests/unit/domain/test_fake_notice_repository.py
@@ -0,0 +1,8 @@
+from ted_sws.notice_fetcher.services.notice_fetcher import NoticeFetcher
+from tests.fakes.fake_repository import FakeNoticeRepository
+
+
+def test_fake_notice_repository():
+    fake_notice_repository = FakeNoticeRepository()
+    #TODO : write test for FakeNoticeRepository
+    #fake_notice_repository.add(notice)

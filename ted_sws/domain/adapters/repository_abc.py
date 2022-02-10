950e4ee6bb8560294e8743eda3a985ef306d26d8
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-02-10 18:09:37 +0200

    Create repository_abc.py

---
+++ ted_sws/domain/adapters/repository_abc.py
@@ -0,0 +1,37 @@
+import abc
+from typing import List
+
+from ted_sws.domain.model.notice import Notice
+
+
+class RepositoryABC(abc.ABC):
+    """
+       This class implements a common interface for all repositories.
+    """
+
+class NoticeRepositoryABC(RepositoryABC):
+    """
+       This repository is intended for storing Notice objects.
+    """
+    @abc.abstractmethod
+    def add(self, notice: Notice):
+        """
+            This method allows you to add notice objects to the repository.
+        :param notice:
+        :return:
+        """
+
+    @abc.abstractmethod
+    def get(self, reference)->Notice:
+        """
+            This method allows a notice to be obtained based on an identification reference.
+        :param reference:
+        :return:
+        """
+
+    @abc.abstractmethod
+    def list(self)->List[Notice]:
+        """
+            This method allows all records to be retrieved from the repository.
+        :return:
+        """

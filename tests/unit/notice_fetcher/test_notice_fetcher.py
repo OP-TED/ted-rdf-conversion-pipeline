07611d18078cb577deac2fe1374e1d0099d6e4e4
Dragos0000 <dragos.paun0000@gmail.com>
2022-02-12 15:32:37 +0000

    fixed merge conflicts

--- tests/unit/notice_fetcher/test_notice_fetcher.py
+++ tests/unit/notice_fetcher/test_notice_fetcher.py
@@ -2,6 +2,7 @@ import datetime
 from ted_sws.domain.model.notice import Notice, NoticeStatus
 from ted_sws.notice_fetcher.services.notice_fetcher import NoticeFetcher
 
+
 def test_notice_fetcher_by_identifier(ted_document_search):
     document_id = "067623-2022"
     notice = NoticeFetcher(ted_api_adapter=ted_document_search).get_notice_by_id(
@@ -38,8 +39,10 @@ def test_notice_fetcher_by_date_range(ted_document_search):
     assert isinstance(notices[0], Notice)
     assert xml_text in notices[0].xml_manifestation.object_data
 
+
 def test_notice_fetcher_by_date_wild_card(ted_document_search):
-    notices = NoticeFetcher(document_search=ted_document_search).get_notices_by_date_wild_card(wildcard_date="20220203*")
+    notices = NoticeFetcher(ted_api_adapter=ted_document_search).get_notices_by_date_wild_card(
+        wildcard_date="20220203*")
     xml_text = "<NOTICE_DATA>"
 
     assert isinstance(notices, list)

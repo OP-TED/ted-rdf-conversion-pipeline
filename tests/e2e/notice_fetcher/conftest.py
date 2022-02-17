623257627d7602772b35be427c530aa37b910b3c
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-02-17 14:55:11 +0200

    Update conftest.py

--- tests/e2e/notice_fetcher/conftest.py
+++ tests/e2e/notice_fetcher/conftest.py
@@ -1,12 +1,7 @@
 import pytest
 
 from ted_sws.notice_fetcher.adapters.ted_api import TedAPIAdapter, TedRequestAPI
-from tests.fakes.fake_repository import FakeNoticeRepository
 
-
-@pytest.fixture
-def notice_repository():
-    return FakeNoticeRepository()
 
 @pytest.fixture
 def ted_document_search():

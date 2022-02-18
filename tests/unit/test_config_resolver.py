24cf001d7b0ba132fe369f1d22c161e063f98614
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-02-18 19:56:17 +0200

    Update test_config_resolver.py

--- tests/unit/test_config_resolver.py
+++ tests/unit/test_config_resolver.py
@@ -1,6 +1,14 @@
+import os
 from ted_sws import config
+from mfy_data_core.adapters.vault_secrets_store import VaultSecretsStore
+
 
 
 def test_config_resolver():
+    print(os.environ.get('VAULT_ADDR'))
+    print(os.environ.get('VAULT_TOKEN'))
+    print(VaultSecretsStore.default_vault_addr)
+    print(VaultSecretsStore.default_vault_token)
     mongo_db_url = config.MONGO_DB_AUTH_URL
+    print(mongo_db_url)
     assert mongo_db_url

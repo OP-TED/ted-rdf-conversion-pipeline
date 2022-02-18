cd970d8a61b9ccd51c98eb4621c5f2b55f6f612b
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-02-18 18:57:38 +0200

    add config resolver

--- ted_sws/__init__.py
+++ ted_sws/__init__.py
@@ -8,3 +8,30 @@
 """ """
 
 __version__ = "0.0.1"
+import dotenv
+from mfy_data_core.adapters.config_resolver import VaultAndEnvConfigResolver
+from mfy_data_core.adapters.vault_secrets_store import VaultSecretsStore
+
+dotenv.load_dotenv(verbose=True, override=True)
+
+SECRET_PATHS = ['mongo-db']
+SECRET_MOUNT = 'ted'
+
+VaultSecretsStore.default_secret_mount = SECRET_MOUNT
+VaultSecretsStore.default_secret_paths = SECRET_PATHS
+
+
+class MongoDBConfig:
+
+    @property
+    def MONGO_DB_AUTH_URL(self) -> str:
+        return VaultAndEnvConfigResolver().config_resolve()
+
+
+class TedConfigResolver(MongoDBConfig):
+    """
+        This class resolve the secrets of the ted-sws project.
+    """
+
+
+config = TedConfigResolver()

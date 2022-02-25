54d878f06fd8bd831f7f0df39407e629b6551557
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-02-25 18:50:09 +0200

    add unit tests

---
+++ tests/unit/data_manager/test_notice_repository.py
@@ -0,0 +1,25 @@
+import pytest
+
+from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
+from ted_sws.domain.model.manifestation import XMLManifestation
+from ted_sws.domain.model.metadata import TEDMetadata
+from ted_sws.domain.model.notice import Notice
+
+NOTICE_TED_ID = "123456"
+
+
+def test_notice_repository_create(mongodb_client):
+    mongodb_client.drop_database(NoticeRepository._database_name)
+    notice_repository = NoticeRepository(mongodb_client=mongodb_client)
+    notice = Notice(ted_id=NOTICE_TED_ID, original_metadata=TEDMetadata(**{"AA": "HEY HEY"}),
+                    xml_manifestation=XMLManifestation(object_data="HELLO"))
+    notice_repository.add(notice)
+    result_notice = notice_repository.get(reference=NOTICE_TED_ID)
+    assert result_notice
+    assert result_notice.ted_id == NOTICE_TED_ID
+    result_notices = notice_repository.list()
+    assert result_notices
+    assert len(result_notices) == 1
+    with pytest.raises(Exception):
+        notice_repository.add(notice)
+    mongodb_client.drop_database(NoticeRepository._database_name)

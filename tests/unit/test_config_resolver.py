334ef4516ee71014583ed829f00493d0c911f09b
Kolea Plesco <kalean.bl@gmail.com>
2022-03-14 18:48:42 +0200

    Added bulk_packager script to generate test packages for Cellar upload tests

--- tests/unit/test_config_resolver.py
+++ tests/unit/test_config_resolver.py
@@ -2,7 +2,5 @@ from ted_sws import config
 
 
 def test_config_resolver():
-    # mongo_db_url = config.MONGO_DB_AUTH_URL
-    # assert mongo_db_url
-    assert True
-
+    mongo_db_url = config.MONGO_DB_AUTH_URL
+    assert mongo_db_url

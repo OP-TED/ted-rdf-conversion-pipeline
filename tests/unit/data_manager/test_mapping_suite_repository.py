d40b64a066c17b66efc6141d8f3dab527b8a285b
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-03-26 00:00:11 +0200

    Update test_mapping_suite_repository.py

--- tests/unit/data_manager/test_mapping_suite_repository.py
+++ tests/unit/data_manager/test_mapping_suite_repository.py
@@ -13,8 +13,9 @@ def test_mapping_suite_repository_mongodb(mongodb_client, fake_mapping_suite):
     result_mapping_suite.title = "updated_title"
     mapping_suite_repository.update(mapping_suite=result_mapping_suite)
     result_mapping_suite = mapping_suite_repository.get(reference=fake_mapping_suite.identifier)
-    assert result_mapping_suite.title ==  "updated_title"
-    result_mapping_suites =list(mapping_suite_repository.list())
+    assert result_mapping_suite.shacl_test_suites[0].identifier == "fake_shacl_test_suite"
+    assert result_mapping_suite.title == "updated_title"
+    result_mapping_suites = list(mapping_suite_repository.list())
     assert len(result_mapping_suites) == 1
     mongodb_client.drop_database(MappingSuiteRepositoryMongoDB._database_name)
 
@@ -43,8 +44,8 @@ def test_mapping_suite_repository_in_file_system(file_system_repository_path, fa
     result_mapping_suite.title = "updated_title"
     mapping_suite_repository.update(mapping_suite=result_mapping_suite)
     result_mapping_suite = mapping_suite_repository.get(reference=fake_mapping_suite.identifier)
-    assert result_mapping_suite.title ==  "updated_title"
-    result_mapping_suites =list(mapping_suite_repository.list())
+    assert result_mapping_suite.title == "updated_title"
+    result_mapping_suites = list(mapping_suite_repository.list())
     assert len(result_mapping_suites) == 1
     result_mapping_suite.identifier = "new_id"
     mapping_suite_repository.add(mapping_suite=result_mapping_suite)
@@ -52,9 +53,11 @@ def test_mapping_suite_repository_in_file_system(file_system_repository_path, fa
     assert len(result_mapping_suites) == 2
     mapping_suite_repository.clear_repository()
 
+
 def test_inter_transactions_mapping_suite_repositories(mongodb_client, file_system_repository_path, fake_mapping_suite):
     mapping_suite_repository_mongodb = MappingSuiteRepositoryMongoDB(mongodb_client=mongodb_client)
-    mapping_suite_repository_file_system = MappingSuiteRepositoryInFileSystem(repository_path=file_system_repository_path)
+    mapping_suite_repository_file_system = MappingSuiteRepositoryInFileSystem(
+        repository_path=file_system_repository_path)
     mapping_suite_repository_file_system.clear_repository()
     mapping_suite_repository_mongodb.add(mapping_suite=fake_mapping_suite)
     result_mapping_suite = mapping_suite_repository_mongodb.get(reference=fake_mapping_suite.identifier)

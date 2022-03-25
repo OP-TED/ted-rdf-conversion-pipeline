b0ec7bf06e24386364edb4655ccd9e68ae5215b1
Dragos0000 <dragos.paun0000@gmail.com>
2022-03-25 06:45:19 +0000

    review changes 2

--- tests/unit/metadata_normaliser/test_mapping_files_registry.py
+++ tests/unit/metadata_normaliser/test_mapping_files_registry.py
@@ -5,14 +5,14 @@ from ted_sws.metadata_normaliser.resources.mapping_files_registry import Mapping
 
 def test_mapping_file_registry():
     json_resource_files = [MappingFilesRegistry().countries, MappingFilesRegistry().notice_type,
-                      MappingFilesRegistry().languages,
-                      MappingFilesRegistry().legal_basis]
-    for file in json_resource_files:
-        assert isinstance(file, dict)
-        assert "results" in file.keys()
+                           MappingFilesRegistry().languages,
+                           MappingFilesRegistry().legal_basis]
+    for file_content in json_resource_files:
+        assert isinstance(file_content, dict)
+        assert "results" in file_content.keys()
 
-    csv_resource_files = [MappingFilesRegistry().sf_notice_df,MappingFilesRegistry().ef_notice_df]
+    csv_resource_files = [MappingFilesRegistry().sf_notice_df, MappingFilesRegistry().ef_notice_df]
 
-    for file in csv_resource_files:
-        assert isinstance(file, pd.DataFrame)
-        assert "eforms_subtype" in file.keys()
+    for file_content in csv_resource_files:
+        assert isinstance(file_content, pd.DataFrame)
+        assert "eforms_subtype" in file_content.keys()

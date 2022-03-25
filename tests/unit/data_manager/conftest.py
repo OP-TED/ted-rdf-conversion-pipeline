b3884fedd7fa517c69ed74e82a7079e77ff77cd7
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-03-26 00:00:06 +0200

    Update conftest.py

--- tests/unit/data_manager/conftest.py
+++ tests/unit/data_manager/conftest.py
@@ -5,7 +5,7 @@ import pymongo
 import pytest
 
 from ted_sws.domain.model.transform import MetadataConstraints, FileResource, TransformationRuleSet, SHACLTestSuite, \
-    SPARQLTestSuite, MappingSuite
+    SPARQLTestSuite, MappingSuite, TransformationTestData
 from tests import TEST_DATA_PATH
 
 
@@ -27,13 +27,16 @@ def fake_mapping_suite():
     transformation_rule_set = TransformationRuleSet(resources=[empty_file_resource],
                                                     rml_mapping_rules=[empty_file_resource]
                                                     )
-    shacl_test_suite = SHACLTestSuite(shacl_tests=[empty_file_resource])
-    sparql_test_suite = SPARQLTestSuite(purpose="no_purpose",
+    shacl_test_suite = SHACLTestSuite(identifier="fake_shacl_test_suite",
+                                      shacl_tests=[empty_file_resource])
+    sparql_test_suite = SPARQLTestSuite(identifier="fake_sparql_test_suite",
                                         sparql_tests=[empty_file_resource]
                                         )
+    transformation_test_data = TransformationTestData(test_data=[empty_file_resource])
     mapping_suite = MappingSuite(metadata_constraints=metadata_constrains,
                                  transformation_rule_set=transformation_rule_set,
                                  shacl_test_suites=[shacl_test_suite],
-                                 sparql_test_suites=[sparql_test_suite]
+                                 sparql_test_suites=[sparql_test_suite],
+                                 transformation_test_data=transformation_test_data
                                  )
     return mapping_suite

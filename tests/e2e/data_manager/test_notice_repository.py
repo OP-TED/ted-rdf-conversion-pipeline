d25aff96ca2747a417b01387b312a2c1d8a412ca
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-03-28 21:57:17 +0300

    Update test_notice_repository.py

--- tests/e2e/data_manager/test_notice_repository.py
+++ tests/e2e/data_manager/test_notice_repository.py
@@ -6,10 +6,12 @@ from ted_sws.core.model.metadata import TEDMetadata
 from ted_sws.core.model.notice import Notice
 
 NOTICE_TED_ID = "123456"
+TEST_DATABASE_NAME = "test_database_name"
+
 
 def test_notice_repository_create(mongodb_client):
-    mongodb_client.drop_database(NoticeRepository._database_name)
-    notice_repository = NoticeRepository(mongodb_client=mongodb_client)
+    mongodb_client.drop_database(TEST_DATABASE_NAME)
+    notice_repository = NoticeRepository(mongodb_client=mongodb_client, database_name=TEST_DATABASE_NAME)
     notice = Notice(ted_id=NOTICE_TED_ID, original_metadata=TEDMetadata(**{"AA": "Metadata"}),
                     xml_manifestation=XMLManifestation(object_data="HELLO"))
     notice_repository.add(notice)
@@ -27,4 +29,4 @@ def test_notice_repository_create(mongodb_client):
     assert result_notice
     assert result_notice.ted_id == NOTICE_TED_ID
     assert result_notice.original_metadata.AA == "Updated metadata"
-    mongodb_client.drop_database(NoticeRepository._database_name)
+    mongodb_client.drop_database(TEST_DATABASE_NAME)

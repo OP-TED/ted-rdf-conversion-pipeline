bb7e5cbd445e22f5d4d8289224e20595edb72290
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-03-30 22:52:05 +0300

    Update single_notice_processing_pipeline_with_decorators.py

--- dags/single_notice_processing_pipeline_with_decorators.py
+++ dags/single_notice_processing_pipeline_with_decorators.py
@@ -1,5 +1,7 @@
 import sys
 
+from airflow.utils.trigger_rule import TriggerRule
+
 from ted_sws.core.model.notice import NoticeStatus
 
 sys.path.append("/opt/airflow/")
@@ -29,28 +31,32 @@ def push(key, value):
     return context['ti'].xcom_push(key=str(key), value=value)
 
 
+NOTICE_ID = "notice_id"
+MAPPING_SUITE_ID = "mapping_suite_id"
+
 @dag(default_args=DEFAULT_DAG_ARGUMENTS, tags=['worker', 'pipeline'])
 def single_notice_proc_pipeline():
 
+
     def _normalise_notice_metadata():
-        notice_id = pull(NoticeStatus.RAW)
+        notice_id = pull(NOTICE_ID)
         print(notice_id)
         notice_id = notice_id + "_normalised"
-        push(NoticeStatus.NORMALISED_METADATA, notice_id)
+        push(NOTICE_ID, notice_id)
 
     def _check_eligibility_for_transformation():
-        notice_id = pull(NoticeStatus.NORMALISED_METADATA)
+        notice_id = pull(NOTICE_ID)
         print(notice_id)
         notice_id = notice_id + "_checked"
-        push(f"notice_id_{NoticeStatus.ELIGIBLE_FOR_TRANSFORMATION}", notice_id)
-        push(f"mapping_suite_id_{NoticeStatus.ELIGIBLE_FOR_TRANSFORMATION}", "mapping_suite_id")
+        push(NOTICE_ID, notice_id)
+        push(MAPPING_SUITE_ID, "mapping_suite_id")
 
     def _transform_notice():
-        notice_id = pull(f"notice_id_{NoticeStatus.ELIGIBLE_FOR_TRANSFORMATION}")
-        mapping_suite_id = pull(f"mapping_suite_id_{NoticeStatus.ELIGIBLE_FOR_TRANSFORMATION}")
+        notice_id = pull(NOTICE_ID)
+        mapping_suite_id = pull(MAPPING_SUITE_ID)
         print(notice_id, mapping_suite_id)
         notice_id = notice_id + "_transformed"
-        push(NoticeStatus.TRANSFORMED, notice_id)
+        push(NOTICE_ID, notice_id)
 
     state_skip_table = {
         str(NoticeStatus.RAW): "normalise_notice_metadata",
@@ -62,15 +68,18 @@ def single_notice_proc_pipeline():
 
     normalise_notice_metadata = PythonOperator(
         task_id="normalise_notice_metadata",
-        python_callable=_normalise_notice_metadata
+        python_callable=_normalise_notice_metadata,
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
     )
     check_eligibility_for_transformation = PythonOperator(
         task_id="check_eligibility_for_transformation",
-        python_callable=_check_eligibility_for_transformation
+        python_callable=_check_eligibility_for_transformation,
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
     )
     transform_notice = PythonOperator(
         task_id="transform_notice",
-        python_callable=_transform_notice
+        python_callable=_transform_notice,
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
     )
 
     normalise_notice_metadata >> check_eligibility_for_transformation >> transform_notice
@@ -78,7 +87,7 @@ def single_notice_proc_pipeline():
     def _get_task_run():
         context = get_current_context()
         dag_params = context["dag_run"].conf
-        push(key=dag_params["notice_status"], value=dag_params["notice_id"])
+        push(key=NOTICE_ID, value=dag_params["notice_id"])
         return state_skip_table[dag_params["notice_status"]]
 
     branch_task = BranchPythonOperator(

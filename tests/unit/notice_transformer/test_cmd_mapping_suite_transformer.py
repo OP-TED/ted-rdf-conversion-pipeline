654ec45cf91300a1ac1dd4da410edf9d8f49dab5
Kolea Plesco <kalean.bl@gmail.com>
2022-04-01 10:31:55 +0300

    Updated test coverage

--- tests/unit/notice_transformer/test_cmd_mapping_suite_transformer.py
+++ tests/unit/notice_transformer/test_cmd_mapping_suite_transformer.py
@@ -9,7 +9,14 @@ from ted_sws.notice_transformer.entrypoints.cmd_mapping_suite_transformer import
 cmdRunner = CliRunner()
 
 
+def __init(fake_repository_path):
+    makefile_path = fake_repository_path / '../../../..'
+    cmd = f"cd {makefile_path} && make rml-mapper-path-add-dotenv-file"
+    subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+
+
 def test_cmd_transformer(fake_mapping_suite_id, fake_repository_path):
+    __init(fake_repository_path)
     response = cmdRunner.invoke(transform_notice, [fake_mapping_suite_id, "--opt-mappings-path", fake_repository_path])
     assert response.exit_code == 0
     assert fake_mapping_suite_id in response.output
@@ -17,6 +24,7 @@ def test_cmd_transformer(fake_mapping_suite_id, fake_repository_path):
 
 
 def test_cmd_transformer_with_not_package(fake_not_mapping_suite_id, fake_repository_path):
+    __init(fake_repository_path)
     response = cmdRunner.invoke(transform_notice,
                                 [fake_not_mapping_suite_id, "--opt-mappings-path", fake_repository_path])
     assert "FAILED" in response.output
@@ -24,18 +32,21 @@ def test_cmd_transformer_with_not_package(fake_not_mapping_suite_id, fake_reposi
 
 
 def test_cmd_transformer_with_failed_package(fake_failed_mapping_suite_id, fake_repository_path):
+    __init(fake_repository_path)
     response = cmdRunner.invoke(transform_notice,
                                 [fake_failed_mapping_suite_id, "--opt-mappings-path", fake_repository_path])
     assert "FAILED" in response.output
 
 
 def test_cmd_transformer_with_no_suite_id(fake_repository_path, fake_mapping_suite_id):
+    __init(fake_repository_path)
     response = cmdRunner.invoke(transform_notice, ["--opt-mappings-path", fake_repository_path])
     assert response.exit_code == 0
 
 
 def test_cmd_transformer_from_cli(fake_repository_path, cmd_transformer_path, fake_mapping_suite_id):
-    cmd = f"cd {fake_repository_path} && pwd && ln -sf {cmd_transformer_path} transformer && ls -al && ./transformer --opt-mapping-suite-id={fake_mapping_suite_id} --opt-mappings-path=."
+    __init(fake_repository_path)
+    cmd = f"cd {fake_repository_path} && ln -sf {cmd_transformer_path} transformer && ./transformer --opt-mapping-suite-id={fake_mapping_suite_id} --opt-mappings-path=."
     response = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
     print(response.stdout.decode('utf-8'))
     assert response.returncode == 0

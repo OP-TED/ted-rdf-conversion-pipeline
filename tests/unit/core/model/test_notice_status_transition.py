a633ac05c181f2cce4d1f84e1cc4a68051bb8af1
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-04 16:54:19 +0300

    Update test_notice_status_transition.py

--- tests/unit/core/model/test_notice_status_transition.py
+++ tests/unit/core/model/test_notice_status_transition.py
@@ -100,3 +100,19 @@ def test_notice_status_transition_below_normalised_metadata(publicly_available_n
     assert publicly_available_notice.normalised_metadata is None
     assert publicly_available_notice.xml_manifestation is not None
     assert publicly_available_notice.original_metadata is not None
+
+
+def test_notice_status_transition_check_preprocessed_and_distilled_state(raw_notice, publicly_available_notice):
+    """
+    
+    :param raw_notice:
+    :param publicly_available_notice:
+    :return:
+    """
+    assert raw_notice.status == NoticeStatus.RAW
+    raw_notice._status = NoticeStatus.ELIGIBLE_FOR_TRANSFORMATION
+    raw_notice.set_preprocessed_xml_manifestation(preprocessed_xml_manifestation=publicly_available_notice.preprocessed_xml_manifestation)
+    assert raw_notice.status == NoticeStatus.PREPROCESSED_FOR_TRANSFORMATION
+    raw_notice.update_status_to(NoticeStatus.TRANSFORMED)
+    raw_notice.set_distilled_rdf_manifestation(distilled_rdf_manifestation=publicly_available_notice.distilled_rdf_manifestation)
+    assert raw_notice.status == NoticeStatus.DISTILLED

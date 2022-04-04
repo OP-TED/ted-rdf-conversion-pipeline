80eaeb23a1ac05723548ff58740189b39ac22fe3
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-04 16:49:28 +0300

    Update selector_retransform_process_orchestrator.py

--- dags/selector_retransform_process_orchestrator.py
+++ dags/selector_retransform_process_orchestrator.py
@@ -1,4 +1,7 @@
 import sys
+
+from ted_sws.core.model.notice import NoticeStatus
+
 sys.path.append("/opt/airflow/")
 sys.path = list(set(sys.path))
 import os
@@ -16,7 +19,7 @@ from ted_sws.notice_fetcher.adapters.ted_api import TedAPIAdapter, TedRequestAPI
 from ted_sws.notice_fetcher.services.notice_fetcher import NoticeFetcher
 
 @dag(default_args=DEFAULT_DAG_ARGUMENTS, tags=['selector', 'daily-fetch'])
-def selector_daily_fetch_orchestrator():
+def selector_re_transform_process_orchestrator():
     @task
     def fetch_notice_from_ted():
         context = get_current_context()
@@ -38,16 +41,18 @@ def selector_daily_fetch_orchestrator():
         context = get_current_context()
         mongodb_client = MongoClient(config.MONGO_DB_AUTH_URL)
         notice_repository = NoticeRepository(mongodb_client=mongodb_client)
-        notices = notice_repository.list()
+        notices = notice_repository.get_notice_by_status(notice_status=NoticeStatus.RAW)
         for notice in notices:
             TriggerDagRunOperator(
                 task_id=f'trigger_worker_dag_{notice.ted_id}',
-                trigger_dag_id="document_proc_pipeline",
-                conf={"notice_id": notice.ted_id}
+                trigger_dag_id="worker_single_notice_process_orchestrator",
+                conf={"notice_id": notice.ted_id,
+                      "notice_status": notice.status
+                      }
             ).execute(context=context)
 
     fetch_notice_from_ted()
     trigger_document_proc_pipeline()
 
 
-etl_dag = selector_daily_fetch_orchestrator()
+etl_dag = selector_re_transform_process_orchestrator()

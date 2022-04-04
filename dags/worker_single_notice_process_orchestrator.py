0effcc9f9b3931c6434c02150c9e646288dd8822
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-04 19:00:47 +0300

    Update worker_single_notice_process_orchestrator.py

--- dags/worker_single_notice_process_orchestrator.py
+++ dags/worker_single_notice_process_orchestrator.py
@@ -50,6 +50,15 @@ def worker_single_notice_process_orchestrator():
         push(NOTICE_ID, notice_id)
         push(MAPPING_SUITE_ID, "mapping_suite_id")
 
+
+    def _preprocess_xml_manifestation():
+        notice_id = pull(NOTICE_ID)
+        mapping_suite_id = pull(MAPPING_SUITE_ID)
+        print(notice_id, mapping_suite_id)
+        notice_id = notice_id + "_preprocessed"
+        push(NOTICE_ID, notice_id)
+        pull(MAPPING_SUITE_ID, mapping_suite_id)
+
     def _transform_notice():
         notice_id = pull(NOTICE_ID)
         mapping_suite_id = pull(MAPPING_SUITE_ID)
@@ -138,6 +147,13 @@ def worker_single_notice_process_orchestrator():
         python_callable=_check_eligibility_for_transformation,
         trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
     )
+
+    preprocess_xml_manifestation = PythonOperator(
+        task_id="preprocess_xml_manifestation",
+        python_callable=_preprocess_xml_manifestation,
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
+    )
+
     transform_notice = PythonOperator(
         task_id="transform_notice",
         python_callable=_transform_notice,
@@ -220,8 +236,8 @@ def worker_single_notice_process_orchestrator():
 
 
 
-    normalise_notice_metadata >> check_eligibility_for_transformation >> check_notice_state_before_transform >> [transform_notice, fail_on_state]
-    transform_notice >> resolve_entities_in_the_rdf_manifestation >> validate_transformed_rdf_manifestation >> check_eligibility_for_packing_by_validation_report
+    normalise_notice_metadata >> check_eligibility_for_transformation >> check_notice_state_before_transform >> [preprocess_xml_manifestation, fail_on_state]
+    preprocess_xml_manifestation >> transform_notice >> resolve_entities_in_the_rdf_manifestation >> validate_transformed_rdf_manifestation >> check_eligibility_for_packing_by_validation_report
     check_eligibility_for_packing_by_validation_report >> check_notice_state_before_generate_mets_package >> [generate_mets_package, fail_on_state]
     generate_mets_package >> check_package_integrity_by_package_structure >> check_notice_state_before_publish_notice_in_cellar >> [publish_notice_in_cellar, fail_on_state]
     publish_notice_in_cellar >> check_notice_public_availability_in_cellar >> check_notice_state_before_notice_successfully_processed >> [notice_successfully_processed, fail_on_state]
@@ -250,7 +266,7 @@ def worker_single_notice_process_orchestrator():
         python_callable=_get_task_run,
     )
 
-    branch_task >> [normalise_notice_metadata, check_eligibility_for_transformation, transform_notice, generate_mets_package, publish_notice_in_cellar]
+    branch_task >> [normalise_notice_metadata, check_eligibility_for_transformation, generate_mets_package, publish_notice_in_cellar]
 
 
 dag = worker_single_notice_process_orchestrator()

25c0acf1af5a7e92097684b97a2d3f9d86ce5683
Dragos0000 <dragos.paun0000@gmail.com>
2022-04-05 15:57:20 +0100

    added feature tests

--- tests/features/notice_fetcher/conftest.py
+++ tests/features/notice_fetcher/conftest.py
@@ -1,19 +1,4 @@
 import pytest
-from pymongo import MongoClient
-
-from ted_sws import config
-from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
-from ted_sws.notice_fetcher.adapters.ted_api import DEFAULT_TED_API_URL
-
-
-NOTICE_STORAGE_FEATURES_TEST_DB = "features_test_db_for_notice"
-
-@pytest.fixture
-def notice_storage():
-    url = config.MONGO_DB_AUTH_URL
-    mongodb_client = MongoClient(url)
-    mongodb_client.drop_database(NOTICE_STORAGE_FEATURES_TEST_DB)
-    return NoticeRepository(mongodb_client=mongodb_client, database_name=NOTICE_STORAGE_FEATURES_TEST_DB)
 
 
 @pytest.fixture
@@ -31,6 +16,3 @@ def notice_incorrect_search_query():
     return {"q": "ND=067623-20224856"}
 
 
-@pytest.fixture
-def api_end_point():
-    return DEFAULT_TED_API_URL

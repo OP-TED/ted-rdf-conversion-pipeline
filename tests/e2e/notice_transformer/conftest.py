4b04070565c37a1491d1f311b53540c98f72aa28
Kolea Plesco <kalean.bl@gmail.com>
2022-04-06 13:24:40 +0300

    Merge conflicts updates

--- tests/e2e/notice_transformer/conftest.py
+++ tests/e2e/notice_transformer/conftest.py
@@ -7,4 +7,4 @@ from tests import TEST_DATA_PATH
 
 @pytest.fixture
 def rml_test_package_path() -> pathlib.Path:
-    return TEST_DATA_PATH / "notice_transformer" / "mappings" / "test_package"
+    return TEST_DATA_PATH / "notice_transformer" / "test_repository" / "test_package"

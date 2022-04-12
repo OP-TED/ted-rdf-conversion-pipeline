80858507771d59af2a8ea775d17ed5ee551cff9f
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-13 00:11:23 +0300

    Update conftest.py

--- tests/unit/mapping_suite_processor/conftest.py
+++ tests/unit/mapping_suite_processor/conftest.py
@@ -6,3 +6,8 @@ from tests import TEST_DATA_PATH
 @pytest.fixture
 def file_system_repository_path():
     return TEST_DATA_PATH / "notice_transformer" / "mapping_suite_processor_repository"
+
+@pytest.fixture
+def yarrrml_file_path():
+    
+    return None

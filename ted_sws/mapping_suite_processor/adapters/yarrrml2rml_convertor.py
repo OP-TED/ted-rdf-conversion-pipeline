fec55c0c800467e1e39e530efabf008f883b29a6
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-13 00:10:47 +0300

    Update yarrrml2rml_convertor.py

--- ted_sws/mapping_suite_processor/adapters/yarrrml2rml_convertor.py
+++ ted_sws/mapping_suite_processor/adapters/yarrrml2rml_convertor.py
@@ -1,5 +1,6 @@
 import abc
 import pathlib
+import subprocess
 
 
 class YARRRML2RMLConvertorABC(abc.ABC):
@@ -15,5 +16,18 @@ class YARRRML2RMLConvertorABC(abc.ABC):
         :param rml_output_file_path:
         :return:
         """
-        
 
+
+class YARRRML2RMLConvertor(YARRRML2RMLConvertorABC):
+    """
+
+    """
+    def convert(self, yarrrml_input_file_path: pathlib.Path, rml_output_file_path: pathlib.Path):
+        """
+
+        :param yarrrml_input_file_path:
+        :param rml_output_file_path:
+        :return:
+        """
+        bash_script = f"(docker run --rm -it -v {yarrrml_input_file_path.parent}:/data rmlio/yarrrml-parser:latest -i /data/{yarrrml_input_file_path.name}) > {rml_output_file_path}"
+        subprocess.run(bash_script, shell=True, stdout=subprocess.PIPE)

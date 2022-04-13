f2c48d4812e70c54cef869712b0a78e8ef349c5e
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-13 18:27:20 +0300

    Update yarrrml2rml_convertor.py

--- ted_sws/mapping_suite_processor/adapters/yarrrml2rml_convertor.py
+++ ted_sws/mapping_suite_processor/adapters/yarrrml2rml_convertor.py
@@ -5,13 +5,13 @@ import subprocess
 
 class YARRRML2RMLConvertorABC(abc.ABC):
     """
-
+        This class is a general interface of a YARRRML to RML converter.
     """
 
     @abc.abstractmethod
     def convert(self, yarrrml_input_file_path: pathlib.Path, rml_output_file_path: pathlib.Path):
         """
-
+            This method converts a yarrrml file and writes the result to another rml file.
         :param yarrrml_input_file_path:
         :param rml_output_file_path:
         :return:
@@ -20,11 +20,11 @@ class YARRRML2RMLConvertorABC(abc.ABC):
 
 class YARRRML2RMLConvertor(YARRRML2RMLConvertorABC):
     """
-
+        This class converts YARRRML to RML using an external docker container that performs conversion logic.
     """
     def convert(self, yarrrml_input_file_path: pathlib.Path, rml_output_file_path: pathlib.Path):
         """
-
+            This method converts a yarrrml file and writes the result to another rml file.
         :param yarrrml_input_file_path:
         :param rml_output_file_path:
         :return:

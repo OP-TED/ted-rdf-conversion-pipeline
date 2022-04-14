44ae7ccad467302412b469d023142d9fa06f48f7
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-14 22:59:50 +0300

    Update test_yarrrml2rml_convertor.py

--- tests/e2e/mapping_suite_processor/test_yarrrml2rml_convertor.py
+++ tests/e2e/mapping_suite_processor/test_yarrrml2rml_convertor.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 from tempfile import NamedTemporaryFile
 
-from ted_sws.mapping_suite_processor.adapters.yarrrml2rml_convertor import YARRRML2RMLConvertor
+from ted_sws.mapping_suite_processor.adapters.yarrrml2rml_converter import YARRRML2RMLConverter
 
 
 def test_yarrrml_to_rml_convertor(yarrrml_file_content, rml_file_result):
@@ -12,7 +12,7 @@ def test_yarrrml_to_rml_convertor(yarrrml_file_content, rml_file_result):
     rml_file_path = pathlib.Path(rml_file.name)
     yarrrml_file.write(yarrrml_file_content)
     yarrrml_file.seek(0, os.SEEK_SET)
-    yarrrml2rml_convertor = YARRRML2RMLConvertor()
+    yarrrml2rml_convertor = YARRRML2RMLConverter()
     yarrrml2rml_convertor.convert(yarrrml_input_file_path = yarrrml_file_path,
                                   rml_output_file_path= rml_file_path
                                   )

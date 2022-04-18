04c525981d6ab1d6714d326f3392aee2b725de2b
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-19 00:44:03 +0300

    Update conceptual_mapping_processor.py

--- ted_sws/mapping_suite_processor/services/conceptual_mapping_processor.py
+++ ted_sws/mapping_suite_processor/services/conceptual_mapping_processor.py
@@ -3,7 +3,8 @@ import pathlib
 from ted_sws.data_manager.adapters.mapping_suite_repository import TRANSFORM_PACKAGE_NAME, VALIDATE_PACKAGE_NAME, \
     SPARQL_PACKAGE_NAME, METADATA_FILE_NAME, RESOURCES_PACKAGE_NAME, SHACL_PACKAGE_NAME
 from ted_sws.mapping_suite_processor.services.conceptual_mapping_files_injection import \
-    mapping_suite_processor_inject_resources, mapping_suite_processor_inject_shacl_shapes
+    mapping_suite_processor_inject_resources, mapping_suite_processor_inject_shacl_shapes, \
+    mapping_suite_processor_inject_sparql_queries
 from ted_sws.mapping_suite_processor.services.conceptual_mapping_generate_metadata import \
     mapping_suite_processor_generate_metadata
 from ted_sws.mapping_suite_processor.services.conceptual_mapping_generate_sparql_queries import \
@@ -16,6 +17,18 @@ SHACL_SHAPE_INJECTION_FOLDER = "ap_data_shape"
 SHACL_SHAPE_RESOURCES_FOLDER = "shacl_shapes"
 SHACL_SHAPE_FILE_NAME = "ePO_shacl_shapes.rdf"
 MAPPING_FILES_RESOURCES_FOLDER = "mapping_files"
+SPARQL_QUERIES_RESOURCES_FOLDER = "queries"
+SPARQL_QUERIES_INJECTION_FOLDER = "business_queries"
+
+
+def mapping_suite_processor_zip_package(mapping_suite_package_path: pathlib.Path,
+                                        output_archive_file_path: pathlib.Path):
+    """
+        Această funcție 
+    :param mapping_suite_package_path:
+    :param output_archive_file_path:
+    :return:
+    """
 
 
 def mapping_suite_processor_expand_package(mapping_suite_package_path: pathlib.Path):
@@ -31,6 +44,8 @@ def mapping_suite_processor_expand_package(mapping_suite_package_path: pathlib.P
     mapping_files_resources_folder_path = RESOURCES_PATH / MAPPING_FILES_RESOURCES_FOLDER
     shacl_shape_file_path = RESOURCES_PATH / SHACL_SHAPE_RESOURCES_FOLDER / SHACL_SHAPE_FILE_NAME
     shacl_shape_injection_folder = mapping_suite_package_path / VALIDATE_PACKAGE_NAME / SHACL_PACKAGE_NAME / SHACL_SHAPE_INJECTION_FOLDER
+    sparql_queries_resources_folder_path = RESOURCES_PATH / SPARQL_QUERIES_RESOURCES_FOLDER
+    sparql_queries_injection_folder = mapping_suite_package_path / VALIDATE_PACKAGE_NAME / SPARQL_PACKAGE_NAME / SPARQL_QUERIES_INJECTION_FOLDER
     shacl_shape_injection_folder.mkdir(parents=True, exist_ok=True)
     cm_sparql_folder_path.mkdir(parents=True, exist_ok=True)
     resources_folder_path.mkdir(parents=True, exist_ok=True)
@@ -49,5 +64,8 @@ def mapping_suite_processor_expand_package(mapping_suite_package_path: pathlib.P
                                              )
 
     mapping_suite_processor_inject_shacl_shapes(shacl_shape_file_path=shacl_shape_file_path,
-                                                output_resources_folder_path = shacl_shape_injection_folder
+                                                output_shacl_shape_folder_path=shacl_shape_injection_folder
                                                 )
+    mapping_suite_processor_inject_sparql_queries(sparql_queries_folder_path=sparql_queries_resources_folder_path,
+                                                  output_sparql_queries_folder_path=sparql_queries_injection_folder
+                                                  )

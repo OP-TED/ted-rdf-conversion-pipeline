276c6c4de3cc133d494d28a973ca34977ce1ac0a
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-19 14:44:20 +0300

    Update conftest.py

--- tests/unit/mapping_suite_processor/conftest.py
+++ tests/unit/mapping_suite_processor/conftest.py
@@ -1,3 +1,5 @@
+import mongomock
+import pymongo
 import pytest
 
 from tests import TEST_DATA_PATH
@@ -6,3 +8,8 @@ from tests import TEST_DATA_PATH
 @pytest.fixture
 def file_system_repository_path():
     return TEST_DATA_PATH / "notice_transformer" / "mapping_suite_processor_repository"
+
+@pytest.fixture
+@mongomock.patch(servers=(('server.example.com', 27017),))
+def mongodb_client():
+    return pymongo.MongoClient('server.example.com')

ad30f5d407b309d4b2eab453a4dbccae531e3712
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-20 20:58:38 +0300

    Update conceptual_mapping_generate_metadata.py

--- ted_sws/mapping_suite_processor/services/conceptual_mapping_generate_metadata.py
+++ ted_sws/mapping_suite_processor/services/conceptual_mapping_generate_metadata.py
@@ -37,9 +37,15 @@ def generate_metadata(raw_metadata: dict) -> str:
     :param raw_metadata:
     :return:
     """
-    constraints = {FORM_NUMBER_KEY: raw_metadata[FORM_NUMBER_FIELD], LEGAL_BASIS_KEY: raw_metadata[LEGAL_BASIS_FIELD],
-                   YEAR_KEY: raw_metadata[YEAR_FIELD], NOTICE_TYPE_KEY: raw_metadata[NOTICE_TYPE_FIELD],
-                   FORM_TYPE_KEY: raw_metadata[FORM_TYPE_FIELD]}
+
+    def get_list_from_raw_metadata(field_key: str) -> list:
+        return [x.strip() for x in raw_metadata[field_key][0].split(',')]
+
+    constraints = {FORM_NUMBER_KEY: get_list_from_raw_metadata(FORM_NUMBER_FIELD),
+                   LEGAL_BASIS_KEY: get_list_from_raw_metadata(LEGAL_BASIS_FIELD),
+                   YEAR_KEY: get_list_from_raw_metadata(YEAR_FIELD),
+                   NOTICE_TYPE_KEY: get_list_from_raw_metadata(NOTICE_TYPE_FIELD),
+                   FORM_TYPE_KEY: get_list_from_raw_metadata(FORM_TYPE_FIELD)}
 
     metadata = {TITLE_KEY: raw_metadata[TITLE_FIELD][0], IDENTIFIER_KEY: raw_metadata[IDENTIFIER_FIELD][0],
                 CREATED_KEY: datetime.now().isoformat(), VERSION_KEY: raw_metadata[VERSION_FIELD][0],

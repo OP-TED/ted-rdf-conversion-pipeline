47d141b2c5be823291dc5487a7c6001ae4b38695
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-20 20:25:15 +0300

    Create test_mapping_suite_processor_from_github_expand_and_load_package_in_mongo_db.py

---
+++ tests/unit/mapping_suite_processor/test_mapping_suite_processor_from_github_expand_and_load_package_in_mongo_db.py
@@ -0,0 +1,15 @@
+from ted_sws.data_manager.adapters.mapping_suite_repository import MappingSuiteRepositoryMongoDB
+from ted_sws.mapping_suite_processor.services.conceptual_mapping_processor import \
+    mapping_suite_processor_from_github_expand_and_load_package_in_mongo_db
+
+MAPPING_SUITE_PACKAGE_NAME = "package_F03_test"
+MAPPING_SUITE_PACKAGE_ID = "test_package"
+
+def test_mapping_suite_processor_from_github_expand_and_load_package_in_mongo_db(mongodb_client):
+    mapping_suite_processor_from_github_expand_and_load_package_in_mongo_db(
+        mapping_suite_package_name=MAPPING_SUITE_PACKAGE_NAME,
+        mongodb_client=mongodb_client
+    )
+    mapping_suite_repository = MappingSuiteRepositoryMongoDB(mongodb_client=mongodb_client)
+    mapping_suite = mapping_suite_repository.get(reference=MAPPING_SUITE_PACKAGE_ID)
+    assert mapping_suite

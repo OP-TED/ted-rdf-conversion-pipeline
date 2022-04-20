55be5b9934a2f8071a56c7ce4563b063fd9c6fdb
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-20 20:24:37 +0300

    Update github_package_downloader.py

--- ted_sws/mapping_suite_processor/adapters/github_package_downloader.py
+++ ted_sws/mapping_suite_processor/adapters/github_package_downloader.py
@@ -4,16 +4,18 @@ import shutil
 import subprocess
 import tempfile
 
+GITHUB_TED_SWS_ARTEFACTS_MAPPINGS_PATH = "ted-sws-artefacts/mappings"
+
 
 class MappingSuitePackageDownloaderABC(abc.ABC):
     """
-
+        This class is intended to download mapping_suite_package from external resources.
     """
 
     @abc.abstractmethod
     def download(self, mapping_suite_package_name: str, output_mapping_suite_package_path: pathlib.Path):
         """
-
+            This method downloads a mapping_suite_package and loads it at the output_mapping_suite_package_path provided.
         :param mapping_suite_package_name:
         :param output_mapping_suite_package_path:
         :return:
@@ -22,27 +24,29 @@ class MappingSuitePackageDownloaderABC(abc.ABC):
 
 class GitHubMappingSuitePackageDownloader(MappingSuitePackageDownloaderABC):
     """
-
+        This class downloads mapping_suite_package from GitHub.
     """
 
-    def __init__(self, github_repository_mappings_folder_url: str):
+    def __init__(self, github_repository_url: str):
         """
 
-        :param github_repository_mappings_folder_url:
+        :param github_repository_url:
         """
-        self.github_repository_mappings_folder_url = github_repository_mappings_folder_url
+        self.github_repository_url = github_repository_url
 
     def download(self, mapping_suite_package_name: str, output_mapping_suite_package_path: pathlib.Path):
         """
-
+            This method downloads a mapping_suite_package and loads it at the output_mapping_suite_package_path provided.
         :param mapping_suite_package_name:
         :param output_mapping_suite_package_path:
         :return:
         """
-        github_mapping_suite_package_url = f"{self.github_repository_mappings_folder_url}/{mapping_suite_package_name}"
         with tempfile.TemporaryDirectory() as tmp_dir:
             temp_dir_path = pathlib.Path(tmp_dir)
-            bash_script = f"gitdir -d {temp_dir_path} {github_mapping_suite_package_url}"
-            subprocess.run(bash_script, shell=True, stdout=subprocess.PIPE)
-            downloaded_tmp_mapping_suite_path = temp_dir_path / "mappings" / mapping_suite_package_name
-            shutil.copytree(downloaded_tmp_mapping_suite_path, output_mapping_suite_package_path, dirs_exist_ok=True)
+            bash_script = f"cd {temp_dir_path} && git clone {self.github_repository_url}"
+            subprocess.run(bash_script, shell=True,
+                           stdout=subprocess.DEVNULL,
+                           stderr=subprocess.STDOUT)
+            downloaded_tmp_mapping_suite_path = temp_dir_path / GITHUB_TED_SWS_ARTEFACTS_MAPPINGS_PATH / mapping_suite_package_name
+            mapping_suite_package_path = output_mapping_suite_package_path / mapping_suite_package_name
+            shutil.copytree(downloaded_tmp_mapping_suite_path, mapping_suite_package_path, dirs_exist_ok=True)

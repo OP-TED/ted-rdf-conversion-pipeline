1adc4e126d0e6d8a1438f73d85095f7428a65083
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-04-22 22:40:54 +0300

    Update test_notice_transformer.py

--- tests/unit/notice_transformer/test_notice_transformer.py
+++ tests/unit/notice_transformer/test_notice_transformer.py
@@ -3,8 +3,9 @@ import tempfile
 from collections import Counter
 
 from ted_sws.core.model.notice import NoticeStatus
+from ted_sws.data_manager.adapters.mapping_suite_repository import MappingSuiteRepositoryMongoDB
 from ted_sws.notice_transformer.services.notice_transformer import NoticeTransformer, transform_notice, \
-    transform_test_data
+    transform_test_data, transform_notice_by_id
 
 
 def test_notice_transformer(fake_rml_mapper, fake_mapping_suite, notice_2018):
@@ -17,6 +18,19 @@ def test_notice_transformer(fake_rml_mapper, fake_mapping_suite, notice_2018):
 def test_notice_transformer_function(fake_rml_mapper, fake_mapping_suite, notice_2018):
     notice_2018._status = NoticeStatus.PREPROCESSED_FOR_TRANSFORMATION
     result_notice = transform_notice(notice_2018, fake_mapping_suite, fake_rml_mapper)
+    assert result_notice.status == NoticeStatus.TRANSFORMED
+
+
+def test_notice_transformer_by_id_function(fake_rml_mapper, mongodb_client, fake_mapping_suite, notice_2018,
+                                           notice_repository):
+    notice_2018._status = NoticeStatus.PREPROCESSED_FOR_TRANSFORMATION
+    notice_repository.add(notice=notice_2018)
+    notice_id = notice_2018.ted_id
+    mapping_suite_repository = MappingSuiteRepositoryMongoDB(mongodb_client=mongodb_client)
+    mapping_suite_repository.add(mapping_suite=fake_mapping_suite)
+    transform_notice_by_id(notice_id, fake_mapping_suite.identifier, notice_repository, mapping_suite_repository,
+                           fake_rml_mapper)
+    result_notice = notice_repository.get(reference=notice_id)
     assert result_notice.status == NoticeStatus.TRANSFORMED
 
 

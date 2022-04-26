ee31333d8f4b6173eadd9026b1c0850678d46f51
Kolea Plesco <kalean.bl@gmail.com>
2022-04-26 23:34:56 +0300

    Updated mapping_suite_transformer test

--- tests/e2e/notice_transformer/conftest.py
+++ tests/e2e/notice_transformer/conftest.py
@@ -1,4 +1,4 @@
-import pathlib
+from pathlib import Path
 
 import pytest
 
@@ -6,10 +6,35 @@ from tests import TEST_DATA_PATH
 
 
 @pytest.fixture
-def rml_test_package_path() -> pathlib.Path:
+def rml_test_package_path() -> Path:
     return TEST_DATA_PATH / "notice_transformer" / "test_repository" / "test_package"
 
 
 @pytest.fixture
-def rml_non_existing_test_package_path() -> pathlib.Path:
+def rml_non_existing_test_package_path() -> Path:
     return TEST_DATA_PATH / "notice_transformer" / "test_repository" / "non_existing_test_package"
+
+
+@pytest.fixture
+def fake_mapping_suite_id() -> str:
+    return "test_package"
+
+
+@pytest.fixture
+def fake_not_mapping_suite_id() -> str:
+    return "test_not_package"
+
+
+@pytest.fixture
+def fake_failed_mapping_suite_id() -> str:
+    return "test_failed_package"
+
+
+@pytest.fixture
+def fake_fail_repository_path() -> Path:
+    return TEST_DATA_PATH / "notice_transformer" / "test_fail_packages"
+
+
+@pytest.fixture
+def fake_repository_path() -> Path:
+    return TEST_DATA_PATH / "notice_transformer" / "test_repository"

0685bd939611fc1e1d640bbbe1e0c7f2947bdf11
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-05-11 20:49:24 +0300

    Update selector_daily_fetch_orchestrator.py

--- dags/selector_daily_fetch_orchestrator.py
+++ dags/selector_daily_fetch_orchestrator.py
@@ -6,27 +6,34 @@ from pymongo import MongoClient
 
 from ted_sws import config
 from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
+from ted_sws.data_sampler.services.notice_xml_indexer import index_notice
 from ted_sws.notice_fetcher.adapters.ted_api import TedAPIAdapter, TedRequestAPI
 from ted_sws.notice_fetcher.services.notice_fetcher import NoticeFetcher
 from ted_sws.core.model.notice import NoticeStatus
+import datetime
 
-@dag(default_args=DEFAULT_DAG_ARGUMENTS, tags=['selector', 'daily-fetch'])
+
+DAILY_DAG_ARGUMENTS = DEFAULT_DAG_ARGUMENTS
+DAILY_DAG_ARGUMENTS["schedule_interval"] = "0 0 * * *"
+
+@dag(default_args=DAILY_DAG_ARGUMENTS, tags=['selector', 'daily-fetch'])
 def selector_daily_fetch_orchestrator():
     @task
     def fetch_notice_from_ted():
-        context = get_current_context()
-        dag_conf = context["dag_run"].conf
-        key = 'fetch_time_filter'
-        if key in dag_conf.keys():
-            print(dag_conf[key])
-            mongodb_client = MongoClient(config.MONGO_DB_AUTH_URL)
-            NoticeFetcher(notice_repository=NoticeRepository(mongodb_client=mongodb_client),
-                          ted_api_adapter=TedAPIAdapter(request_api=TedRequestAPI())).fetch_notices_by_date_wild_card(
-                wildcard_date=dag_conf[key]) #"20220203*"
-        else:
-            print(f"The key={key} is not present in context")
+        current_datetime_wildcard = datetime.datetime.now().strftime("%Y%m%d*")
+        mongodb_client = MongoClient(config.MONGO_DB_AUTH_URL)
+        NoticeFetcher(notice_repository=NoticeRepository(mongodb_client=mongodb_client),
+                      ted_api_adapter=TedAPIAdapter(request_api=TedRequestAPI())).fetch_notices_by_date_wild_card(
+            wildcard_date=current_datetime_wildcard)
 
-
+    @task
+    def index_notices():
+        mongodb_client = MongoClient(config.MONGO_DB_AUTH_URL)
+        notice_repository = NoticeRepository(mongodb_client=mongodb_client)
+        notices = notice_repository.get_notice_by_status(notice_status=NoticeStatus.RAW)
+        for notice in notices:
+            indexed_notice = index_notice(notice=notice)
+            notice_repository.update(notice=indexed_notice)
 
     @task
     def trigger_document_proc_pipeline():
@@ -43,7 +50,7 @@ def selector_daily_fetch_orchestrator():
                       }
             ).execute(context=context)
 
-    fetch_notice_from_ted() >> trigger_document_proc_pipeline()
+    fetch_notice_from_ted() >> index_notices() >> trigger_document_proc_pipeline()
 
 
 dag = selector_daily_fetch_orchestrator()

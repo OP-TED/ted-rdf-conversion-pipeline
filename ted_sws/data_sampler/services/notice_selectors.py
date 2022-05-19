4bd0f462668c9c2a4c8e4328b6091655fedc9d00
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-05-20 00:38:30 +0300

    Update notice_selectors.py

--- ted_sws/data_sampler/services/notice_selectors.py
+++ ted_sws/data_sampler/services/notice_selectors.py
@@ -8,9 +8,9 @@ from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
 NOTICE_METADATA_FORM_NUMBER_FIELD_NAME = "form_number"
 NOTICE_METADATA_EFORMS_SUBTYPE_FIELD_NAME = "eforms_subtype"
 
-def get_notice_ids_by_metadata_field_value(field_name: str, field_value: str, mongodb_client: MongoClient) -> List[str]:
+def get_notice_ids_by_normalised_metadata_field_value(field_name: str, field_value: str, mongodb_client: MongoClient) -> List[str]:
     """
-
+        This function returns a list of notice_ids, according to the value of a field in normalized_metadata.
     :param field_name:
     :param field_value:
     :param mongodb_client:
@@ -29,24 +29,24 @@ def get_notice_ids_by_metadata_field_value(field_name: str, field_value: str, mo
 
 def get_notice_ids_by_form_number(form_number: str, mongodb_client: MongoClient) -> List[str]:
     """
-
+        This function returns a list of notice_ids, according to a form_number.
     :param form_number:
     :param mongodb_client:
     :return:
     """
-    return get_notice_ids_by_metadata_field_value(field_name=NOTICE_METADATA_FORM_NUMBER_FIELD_NAME,
+    return get_notice_ids_by_normalised_metadata_field_value(field_name=NOTICE_METADATA_FORM_NUMBER_FIELD_NAME,
                                                   field_value=form_number,
                                                   mongodb_client=mongodb_client
                                                   )
 
 def get_notice_ids_by_eforms_subtype(eforms_subtype: str, mongodb_client: MongoClient) -> List[str]:
     """
-
+        This function returns a list of notice_ids, according to an eforms_subtype.
     :param eforms_subtype:
     :param mongodb_client:
     :return:
     """
-    return get_notice_ids_by_metadata_field_value(field_name=NOTICE_METADATA_EFORMS_SUBTYPE_FIELD_NAME,
+    return get_notice_ids_by_normalised_metadata_field_value(field_name=NOTICE_METADATA_EFORMS_SUBTYPE_FIELD_NAME,
                                                   field_value=eforms_subtype,
                                                   mongodb_client=mongodb_client
                                                   )

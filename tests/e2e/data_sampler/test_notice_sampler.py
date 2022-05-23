acaab8613a4751964683c0f4f6dbdc50e351f69b
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-05-24 01:29:02 +0300

    Update test_notice_sampler.py

--- tests/e2e/data_sampler/test_notice_sampler.py
+++ tests/e2e/data_sampler/test_notice_sampler.py
@@ -1,11 +1,15 @@
-from ted_sws.resources.mapping_files_registry import MappingFilesRegistry
+import pathlib
+import tempfile
 
-FORM_NUMBER_COLUMN_NAME = "form_number"
-EFORMS_SUBTYPE_COLUMN_NAME = "eforms_subtype"
+from ted_sws.data_sampler.services.notice_sampler import store_notice_samples_in_file_system, \
+    RESULT_NOTICE_SAMPLES_FOLDER_NAME
 
-def test_dummy():
-    sf_notice_df = MappingFilesRegistry().sf_notice_df
-    form_numbers = list(set(sf_notice_df[FORM_NUMBER_COLUMN_NAME].values.tolist()))
-    eforms_subtypes = list(set(sf_notice_df[EFORMS_SUBTYPE_COLUMN_NAME].values.tolist()))
-    print(form_numbers)
-    print(eforms_subtypes)
+
+def test_notice_sampler(mongodb_client):
+    with tempfile.TemporaryDirectory() as tmp_dirname:
+        test_storage_path = pathlib.Path(tmp_dirname)
+        store_notice_samples_in_file_system(mongodb_client=mongodb_client,
+                                            storage_path=test_storage_path,
+                                            top_k=10)
+        result_path = test_storage_path / RESULT_NOTICE_SAMPLES_FOLDER_NAME
+        assert result_path.exists()

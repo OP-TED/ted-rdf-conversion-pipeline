fd22408039c865b88fa6c22ab86ed46c3ce9804f
Dragos0000 <dragos.paun0000@gmail.com>
2022-05-25 15:12:48 +0100

    yet another fix

--- dags/fetch_notices_per_day_worker.py
+++ dags/fetch_notices_per_day_worker.py
@@ -1,3 +1,5 @@
+import datetime
+
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from dags import DEFAULT_DAG_ARGUMENTS
@@ -32,11 +34,20 @@ def fetch_notices_per_day_worker():
                                        request_api=TedRequestAPI())).fetch_notices_by_date_wild_card(
             wildcard_date=dag_conf[DATE_WILD_CARD_KEY])  # "20220203*"
         for notice_id in notice_ids:
-            TriggerDagRunOperator(
-                task_id=f'trigger_index_and_normalise_notice_worker_dag_{notice_id}',
-                trigger_dag_id="index_and_normalise_notice_worker",
-                conf={NOTICE_ID: notice_id}
-            ).execute(context=context)
+            restart_dag_operator = True
+            while restart_dag_operator:
+                restart_dag_operator = False
+                try:
+                    TriggerDagRunOperator(
+                        task_id=f'trigger_index_and_normalise_notice_worker_dag_{notice_id}',
+                        trigger_dag_id="index_and_normalise_notice_worker",
+                        trigger_run_id=notice_id,
+                        execution_date=datetime.datetime.now().replace(tzinfo=datetime.timezone.utc),
+                        conf={NOTICE_ID: notice_id}
+                    ).execute(context=context)
+                except:
+                    restart_dag_operator = True
+                    print("trigger dag operator restarted !!!")
 
     fetch_notices_and_trigger_index_and_normalise_notice_worker()
 

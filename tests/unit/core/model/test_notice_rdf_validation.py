3663fdfe996563350a8302393ca3d59c2fc04157
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-05-27 16:01:58 +0300

    Update test_notice_rdf_validation.py

--- tests/unit/core/model/test_notice_rdf_validation.py
+++ tests/unit/core/model/test_notice_rdf_validation.py
@@ -8,24 +8,21 @@
 """ """
 import pytest
 
-from ted_sws.core.model.manifestation import RDFValidationManifestation
-from ted_sws.core.model.notice import NoticeStatus, UnsupportedStatusTransition
-from ted_sws.notice_validator.model.shacl_test_suite import SHACLSuiteValidationReport, \
+from ted_sws.core.model.manifestation import RDFValidationManifestation, SHACLTestSuiteValidationReport, \
     QueriedSHACLShapeValidationResult
+from ted_sws.core.model.notice import NoticeStatus
 
 
 def test_set_notice_rdf_validation(publicly_available_notice, raw_notice):
     validation = RDFValidationManifestation(object_data="this is a new validation report")
-    shacl_validation = SHACLSuiteValidationReport(object_data="this is a shacl validation report",
-                                                  shacl_test_suite_identifier="shacl_test_suite_id",
+    shacl_validation = SHACLTestSuiteValidationReport(object_data="this is a shacl validation report",
+                                                  test_suite_identifier="shacl_test_suite_id",
                                                   mapping_suite_identifier="mapping_suite_id",
-                                                  validation_result=QueriedSHACLShapeValidationResult())
-    publicly_available_notice.set_rdf_validation(rdf_validation=validation)
-    publicly_available_notice.set_rdf_validation(rdf_validation=validation)
+                                                  validation_results=QueriedSHACLShapeValidationResult())
+
     publicly_available_notice.set_rdf_validation(rdf_validation=shacl_validation)
 
-    publicly_available_notice.set_distilled_rdf_validation(rdf_validation=validation)
-    publicly_available_notice.set_distilled_rdf_validation(rdf_validation=validation)
+
     publicly_available_notice.set_distilled_rdf_validation(rdf_validation=shacl_validation)
     assert publicly_available_notice.status is NoticeStatus.VALIDATED
     assert publicly_available_notice.mets_manifestation is None
@@ -38,16 +35,12 @@ def test_set_notice_rdf_validation(publicly_available_notice, raw_notice):
 
 def test_set_notice_distilled_rdf_validation(publicly_available_notice, raw_notice):
     validation = RDFValidationManifestation(object_data="this is a new validation report")
-    shacl_validation = SHACLSuiteValidationReport(object_data="this is a shacl validation report",
-                                                  shacl_test_suite_identifier="shacl_test_suite_id",
+    shacl_validation = SHACLTestSuiteValidationReport(object_data="this is a shacl validation report",
+                                                  test_suite_identifier="shacl_test_suite_id",
                                                   mapping_suite_identifier="mapping_suite_id",
-                                                  validation_result=QueriedSHACLShapeValidationResult())
-    publicly_available_notice.set_distilled_rdf_validation(rdf_validation=validation)
-    publicly_available_notice.set_distilled_rdf_validation(rdf_validation=validation)
-    publicly_available_notice.set_distilled_rdf_validation(rdf_validation=shacl_validation)
+                                                  validation_results=QueriedSHACLShapeValidationResult())
 
-    publicly_available_notice.set_rdf_validation(rdf_validation=validation)
-    publicly_available_notice.set_rdf_validation(rdf_validation=validation)
+    publicly_available_notice.set_distilled_rdf_validation(rdf_validation=shacl_validation)
     publicly_available_notice.set_rdf_validation(rdf_validation=shacl_validation)
 
     assert publicly_available_notice.status is NoticeStatus.VALIDATED

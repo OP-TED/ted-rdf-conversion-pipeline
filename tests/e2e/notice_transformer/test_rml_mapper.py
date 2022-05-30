308f526409c4791f921addc63200a4596a5dac5d
Dragos0000 <dragos.paun0000@gmail.com>
2022-05-30 13:51:28 +0100

    test fixing, because why not

--- tests/e2e/notice_transformer/test_rml_mapper.py
+++ tests/e2e/notice_transformer/test_rml_mapper.py
@@ -8,7 +8,7 @@ from tests.test_data.notice_transformer import RML_MAPPER_TEST_RDF_RESULT
 def test_rml_mapper(rml_test_package_path):
     rml_mapper = RMLMapper(rml_mapper_path=config.RML_MAPPER_PATH, serialization_format=DEFAULT_SERIALIZATION_FORMAT)
     rdf_result = rml_mapper.execute(package_path=rml_test_package_path)
-    assert rdf_result == RML_MAPPER_TEST_RDF_RESULT
+    assert RML_MAPPER_TEST_RDF_RESULT in rdf_result
 
     serialization_format = SerializationFormat.TURTLE
     rml_mapper.set_serialization_format(serialization_format)

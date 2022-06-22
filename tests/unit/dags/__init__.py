14da8ce58cc72f7d70bac32e311203a76f094107
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-06-22 21:17:09 +0300

    Update __init__.py

--- tests/unit/dags/__init__.py
+++ tests/unit/dags/__init__.py
@@ -1,7 +1,3 @@
-from datetime import timezone
-
-from airflow.models import DagRun, TaskInstance
-from airflow.utils.types import DagRunType
 
 NORMALISE_NOTICE_METADATA_TASK_ID = "normalise_notice_metadata"
 CHECK_ELIGIBILITY_FOR_TRANSFORMATION_TASK_ID = "check_eligibility_for_transformation"
@@ -45,35 +41,3 @@ PUBLISH_BRANCH_TASK_IDS = [
 FULL_BRANCH_TASK_IDS = [START_PROCESSING_NOTICE_TASK_ID,
                         NORMALISE_NOTICE_METADATA_TASK_ID] + TRANSFORM_BRANCH_TASK_IDS + PACKAGE_BRANCH_TASK_IDS + PUBLISH_BRANCH_TASK_IDS
 
-
-def run_task(dag, task, conf: dict, xcom_push_data: dict = None, ignore_first_depends_on_past=True) -> TaskInstance:
-    start_date = dag.default_args["start_date"]
-    end_date = dag.default_args["start_date"]
-    start_date = start_date or task.start_date
-    end_date = end_date or task.end_date or timezone.utcnow()
-
-    info = list(task.dag.iter_dagrun_infos_between(start_date, end_date, align=False))[0]
-    ignore_depends_on_past = info.logical_date == start_date and ignore_first_depends_on_past
-    dr = DagRun(
-        dag_id=task.dag_id,
-        run_id=DagRun.generate_run_id(DagRunType.MANUAL, info.logical_date),
-        run_type=DagRunType.MANUAL,
-        execution_date=info.logical_date,
-        data_interval=info.data_interval,
-        conf=conf
-    )
-    ti = TaskInstance(task, run_id=None)
-    ti.dag_run = dr
-
-    if xcom_push_data is not None:
-        for key, value in xcom_push_data.items():
-            ti.xcom_push(key=str(key), value=value)
-
-    ti.run(
-        mark_success=False,
-        ignore_task_deps=True,
-        ignore_depends_on_past=ignore_depends_on_past,
-        ignore_ti_state=False,
-        test_mode=True,
-    )
-    return ti

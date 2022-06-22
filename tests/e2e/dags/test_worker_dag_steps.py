8880eb7f4150ac554b9fd17a605772d600b224b1
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-06-22 21:16:24 +0300

    Update test_worker_dag_steps.py

--- tests/e2e/dags/test_worker_dag_steps.py
+++ tests/e2e/dags/test_worker_dag_steps.py
@@ -1,5 +1,5 @@
 from ted_sws import config
-from ted_sws.core.model.notice import NoticeStatus
+from ted_sws.core.model.notice import NoticeStatus, Notice
 from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
 from tests.e2e.dags import run_task, START_PROCESSING_NOTICE_TASK_ID, \
     NORMALISE_NOTICE_METADATA_TASK_ID, CHECK_ELIGIBILITY_FOR_TRANSFORMATION_TASK_ID, \
@@ -20,9 +20,10 @@ def execute_dag_step(dag, task_id: str, dag_config: dict, xcom_push_data: dict =
     assert task_instance.state == "success"
 
 
-def check_notice_status(notice_repository: NoticeRepository, notice_id: str, notice_status: NoticeStatus):
+def check_notice_status(notice_repository: NoticeRepository, notice_id: str, notice_status: NoticeStatus) -> Notice:
     notice = notice_repository.get(reference=notice_id)
     assert notice.status == notice_status
+    return notice
 
 
 def test_worker_dag_steps(dag_bag, notice_repository):
@@ -38,8 +39,9 @@ def test_worker_dag_steps(dag_bag, notice_repository):
     execute_dag_step(dag, task_id=START_PROCESSING_NOTICE_TASK_ID, dag_config=dag_config)
 
     execute_dag_step(dag, task_id=NORMALISE_NOTICE_METADATA_TASK_ID, dag_config=dag_config)
-    check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
-                        notice_status=NoticeStatus.NORMALISED_METADATA)
+    notice = check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
+                                 notice_status=NoticeStatus.NORMALISED_METADATA)
+    assert notice.normalised_metadata is not None
 
     execute_dag_step(dag, task_id=CHECK_ELIGIBILITY_FOR_TRANSFORMATION_TASK_ID, dag_config=dag_config)
     check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
@@ -48,18 +50,31 @@ def test_worker_dag_steps(dag_bag, notice_repository):
     execute_dag_step(dag, task_id=CHECK_NOTICE_STATE_BEFORE_TRANSFORM_TASK_ID, dag_config=dag_config)
 
     execute_dag_step(dag, task_id=PREPROCESS_XML_MANIFESTATION_TASK_ID, dag_config=dag_config)
-    check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
-                        notice_status=NoticeStatus.PREPROCESSED_FOR_TRANSFORMATION)
+    notice = check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
+                                 notice_status=NoticeStatus.PREPROCESSED_FOR_TRANSFORMATION)
+
+    assert notice.rdf_manifestation is None
 
     execute_dag_step(dag, task_id=TRANSFORM_NOTICE_TASK_ID, dag_config=dag_config)
-    check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
-                        notice_status=NoticeStatus.TRANSFORMED)
+    notice = check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
+                                 notice_status=NoticeStatus.TRANSFORMED)
+
+    assert notice.rdf_manifestation is not None
+    assert notice.distilled_rdf_manifestation is None
 
     execute_dag_step(dag, task_id=RESOLVE_ENTITIES_IN_THE_RDF_MANIFESTATION_TASK_ID, dag_config=dag_config)
-    check_notice_status(notice_repository=notice_repository, notice_id=notice_id, notice_status=NoticeStatus.DISTILLED)
+    notice = check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
+                                 notice_status=NoticeStatus.DISTILLED)
+
+    assert notice.distilled_rdf_manifestation is not None
 
     execute_dag_step(dag, task_id=VALIDATE_TRANSFORMED_RDF_MANIFESTATION_TASK_ID, dag_config=dag_config)
-    check_notice_status(notice_repository=notice_repository, notice_id=notice_id, notice_status=NoticeStatus.VALIDATED)
+    notice = check_notice_status(notice_repository=notice_repository, notice_id=notice_id, notice_status=NoticeStatus.VALIDATED)
+
+    assert notice.rdf_manifestation.shacl_validations
+    assert notice.rdf_manifestation.sparql_validations
+    assert notice.distilled_rdf_manifestation.shacl_validations
+    assert notice.distilled_rdf_manifestation.sparql_validations
 
     execute_dag_step(dag, task_id=CHECK_ELIGIBILITY_FOR_PACKING_BY_VALIDATION_REPORT_TASK_ID, dag_config=dag_config)
     check_notice_status(notice_repository=notice_repository, notice_id=notice_id,
@@ -67,8 +82,12 @@ def test_worker_dag_steps(dag_bag, notice_repository):
 
     execute_dag_step(dag, task_id=CHECK_NOTICE_STATE_BEFORE_GENERATE_METS_PACKAGE_TASK_ID, dag_config=dag_config)
 
+    assert notice.mets_manifestation is None
+
     execute_dag_step(dag, task_id=GENERATE_METS_PACKAGE_TASK_ID, dag_config=dag_config)
-    check_notice_status(notice_repository=notice_repository, notice_id=notice_id, notice_status=NoticeStatus.PACKAGED)
+    notice = check_notice_status(notice_repository=notice_repository, notice_id=notice_id, notice_status=NoticeStatus.PACKAGED)
+
+    assert notice.mets_manifestation is not None
 
     # TODO: add this steps when publish notice in cellar will work
     # execute_dag_step(dag, task_id=CHECK_PACKAGE_INTEGRITY_BY_PACKAGE_STRUCTURE_TASK_ID, xcom_push_data=XCOM_DEFAULT)

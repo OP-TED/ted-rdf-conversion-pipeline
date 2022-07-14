4854fa811e51869216b5dae8733e6683177f0a42
costezki <costezki.eugen@gmail.com>
2022-07-14 14:11:23 +0200

    updated SHACL shapes, using 3.0.0.beta

--- ted_sws/__init__.py
+++ ted_sws/__init__.py
@@ -13,8 +13,7 @@ import os
 
 import dotenv
 
-from ted_sws.core.adapters.config_resolver import VaultAndEnvConfigResolver
-from ted_sws.core.adapters.vault_secrets_store import VaultSecretsStore
+from ted_sws.core.adapters.config_resolver import EnvConfigResolver
 
 dotenv.load_dotenv(verbose=True, override=True)
 
@@ -24,15 +23,10 @@ RUN_TEST_ENV_VAL = "test"
 
 os.environ[RUN_ENV_NAME] = RUN_ENV_VAL
 
-ENV = os.environ.get("ENVIRONMENT", default="staging")
-if ENV == "local":
-    SECRET_PATHS = []
-else:
-    SECRET_PATHS = ['mongo-db', 'github']
-    SECRET_MOUNT = f'ted-{ENV}'
-    VaultSecretsStore.default_secret_mount = SECRET_MOUNT
-
-VaultSecretsStore.default_secret_paths = SECRET_PATHS
+# SECRET_PATHS = ['mongo-db', 'github']
+# SECRET_MOUNT = f'ted-{ENV}'
+# VaultSecretsStore.default_secret_mount = SECRET_MOUNT
+# VaultSecretsStore.default_secret_paths = SECRET_PATHS
 
 
 class MongoDBConfig:
@@ -41,7 +35,7 @@ class MongoDBConfig:
     def MONGO_DB_AUTH_URL(self) -> str:
         if self.ENVIRONMENT == "dev" and self.AIRFLOW__CORE__EXECUTOR:
             return self.MONGO_DB_AUTH_URL_DEV_CONTAINER
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def MONGO_DB_AUTH_URL_DEV_CONTAINER(self) -> str:
@@ -49,103 +43,103 @@ class MongoDBConfig:
         This variable is to be used only on dev environment when execution is done from a docker container as oppose to
         development host environment
         """
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def ENVIRONMENT(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def AIRFLOW__CORE__EXECUTOR(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def MONGO_DB_PORT(self) -> int:
-        return int(VaultAndEnvConfigResolver().config_resolve())
+        return int(EnvConfigResolver().config_resolve())
 
     @property
     def MONGO_DB_AGGREGATES_DATABASE_NAME(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
 
 class RMLMapperConfig:
 
     @property
     def RML_MAPPER_PATH(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
 
 class AllegroConfig:
     @property
     def AGRAPH_SUPER_USER(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def AGRAPH_SUPER_PASSWORD(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def ALLEGRO_HOST(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
 class ELKConfig:
 
     @property
     def ELK_HOST(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def ELK_PORT(self) -> int:
-        v: str = VaultAndEnvConfigResolver().config_resolve()
+        v: str = EnvConfigResolver().config_resolve()
         return int(v) if v is not None else None
 
     @property
     def ELK_VERSION(self) -> int:
-        v: str = VaultAndEnvConfigResolver().config_resolve()
+        v: str = EnvConfigResolver().config_resolve()
         return int(v) if v is not None else None
 
 
 class LoggingConfig:
     @property
     def MONGO_DB_LOGS_DATABASE_NAME(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def DAG_LOGGER_CONFIG_HANDLERS(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def CLI_LOGGER_CONFIG_HANDLERS(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
     @property
     def LOGGER_LOG_FILENAME(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
 
 class XMLProcessorConfig:
 
     @property
     def XML_PROCESSOR_PATH(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
 
 class GitHubArtefacts:
 
     @property
     def GITHUB_TED_SWS_ARTEFACTS_URL(self) -> str:
-        return VaultAndEnvConfigResolver().config_resolve()
+        return EnvConfigResolver().config_resolve()
 
 
 class API:
     @property
     def ID_MANAGER_API_HOST(self) -> str:
-        v: str = VaultAndEnvConfigResolver().config_resolve()
+        v: str = EnvConfigResolver().config_resolve()
         return v if v else "localhost"
 
     @property
     def ID_MANAGER_API_PORT(self) -> int:
-        v: str = VaultAndEnvConfigResolver().config_resolve()
+        v: str = EnvConfigResolver().config_resolve()
         return int(v) if v else 8000
 
 

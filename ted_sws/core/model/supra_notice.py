e10709134dcb0690bc1a62a1d72c77569fb6206c
costezki <costezki.eugen@gmail.com>
2022-07-16 11:02:58 +0200

    started implementing supra notice object

---
+++ ted_sws/core/model/supra_notice.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+# supra_notice.py
+# Date:  16/07/2022
+# Author: Eugeniu Costetchi
+# Email: costezki.eugen@gmail.com 
+
+""" This module implements aggregates over groups of notices and the appropriate business needs, on those groups """
+import abc
+import json
+from datetime import datetime, date
+from typing import List
+
+from ted_sws.core.model import PropertyBaseModel
+from ted_sws.core.model.manifestation import Manifestation
+
+
+class SupraNotice(PropertyBaseModel, abc.ABC):
+    """
+        This is an arbitrary aggregate over a list of notices.
+    """
+
+    class Config:
+        underscore_attrs_are_private = True
+        validate_assignment = True
+        orm_mode = True
+
+    created_at: datetime = datetime.now()
+
+    notice_ids: List[str]
+
+
+class SupraNoticeValidationReport(Manifestation):
+    """
+        Result of checking whether all the notices published in TED are present in the internal database.
+    """
+    missing_notice_ids: List[str]
+
+    def is_valid(self):
+        if not self.missing_notice_ids:
+            return True
+        return False
+
+
+class DailySupraNotice(SupraNotice):
+    """
+        This is an aggregate over the notices published in TED in a specific day.
+    """
+    notice_publication_day: date = datetime.today()
+    completness_validation: SupraNoticeValidationReport = None

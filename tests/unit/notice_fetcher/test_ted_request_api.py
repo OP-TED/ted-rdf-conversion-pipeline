39093914fb95eb932639085245c049d84444c420
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-07-25 16:28:20 +0300

    add notice_metdata_processor in unit tests

--- tests/unit/notice_fetcher/test_ted_request_api.py
+++ tests/unit/notice_fetcher/test_ted_request_api.py
@@ -1,14 +1,15 @@
 import pytest
 
-from ted_sws.notice_fetcher.adapters.ted_api import TedRequestAPI, DEFAULT_TED_API_URL
+from ted_sws import config
+from ted_sws.notice_fetcher.adapters.ted_api import TedRequestAPI
 
 
 def test_ted_request_api():
     ted_api_request = TedRequestAPI()
-    notice_by_query = ted_api_request(api_url=DEFAULT_TED_API_URL, api_query={"q": "ND=[67623-2022]"})
+    notice_by_query = ted_api_request(api_url=config.TED_API_URL, api_query={"q": "ND=[67623-2022]"})
     assert notice_by_query
     assert isinstance(notice_by_query, dict)
     with pytest.raises(Exception) as e:
-        ted_api_request(api_url=DEFAULT_TED_API_URL, api_query={"q": "INCORRECT PARAMS"})
+        ted_api_request(api_url=config.TED_API_URL, api_query={"q": "INCORRECT PARAMS"})
     assert str(e.value) == "The API call failed with: <Response [500]>"
 
--- tests/unit/notice_metadata_processor/test_eligibility.py
+++ tests/unit/notice_metadata_processor/test_eligibility.py
@@ -5,15 +5,15 @@ from ted_sws.notice_metadata_processor.services.notice_eligibility import check_
     notice_eligibility_checker, notice_eligibility_checker_by_id, transform_version_string_into_int
 
 
-def test_non_eligibility_by_notice(file_system_repository_path, indexed_notice):
-    mapping_suite_repository = MappingSuiteRepositoryInFileSystem(repository_path=file_system_repository_path)
+def test_non_eligibility_by_notice(notice_eligibility_repository_path, indexed_notice):
+    mapping_suite_repository = MappingSuiteRepositoryInFileSystem(repository_path=notice_eligibility_repository_path)
     MetadataNormaliser(notice=indexed_notice).normalise_metadata()
     notice_eligibility_checker(notice=indexed_notice, mapping_suite_repository=mapping_suite_repository)
     assert indexed_notice.status == NoticeStatus.INELIGIBLE_FOR_TRANSFORMATION
 
 
-def test_eligibility_by_notice(file_system_repository_path, notice_2020):
-    mapping_suite_repository = MappingSuiteRepositoryInFileSystem(repository_path=file_system_repository_path)
+def test_eligibility_by_notice(notice_eligibility_repository_path, notice_2020):
+    mapping_suite_repository = MappingSuiteRepositoryInFileSystem(repository_path=notice_eligibility_repository_path)
     MetadataNormaliser(notice=notice_2020).normalise_metadata()
     notice_checker = notice_eligibility_checker(notice=notice_2020, mapping_suite_repository=mapping_suite_repository)
     notice_id, mapping_suite_identifier = notice_checker
@@ -22,10 +22,10 @@ def test_eligibility_by_notice(file_system_repository_path, notice_2020):
     assert notice_2020.status == NoticeStatus.ELIGIBLE_FOR_TRANSFORMATION
 
 
-def test_eligibility_by_notice_id(file_system_repository_path, notice_2020, notice_repository):
+def test_eligibility_by_notice_id(notice_eligibility_repository_path, notice_2020, notice_repository):
     MetadataNormaliser(notice=notice_2020).normalise_metadata()
     notice_repository.add(notice_2020)
-    mapping_suite_repository = MappingSuiteRepositoryInFileSystem(repository_path=file_system_repository_path)
+    mapping_suite_repository = MappingSuiteRepositoryInFileSystem(repository_path=notice_eligibility_repository_path)
     notice_checker = notice_eligibility_checker_by_id(notice_id="408313-2020",
                                                       mapping_suite_repository=mapping_suite_repository,
                                                       notice_repository=notice_repository)
@@ -36,11 +36,10 @@ def test_eligibility_by_notice_id(file_system_repository_path, notice_2020, noti
     assert notice_2020.status == NoticeStatus.ELIGIBLE_FOR_TRANSFORMATION
 
 
-def test_check_mapping_suite(file_system_repository_path, normalised_metadata_object):
-    mapping_suite_repository = MappingSuiteRepositoryInFileSystem(repository_path=file_system_repository_path)
+def test_check_mapping_suite(notice_eligibility_repository_path, normalised_metadata_object):
+    mapping_suite_repository = MappingSuiteRepositoryInFileSystem(repository_path=notice_eligibility_repository_path)
     is_valid = check_package(mapping_suite=mapping_suite_repository.get("test_package"),
                              notice_metadata=normalised_metadata_object)
-
 
     assert isinstance(is_valid, bool)
     assert is_valid

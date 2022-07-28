040de6260d4a7806b28b427204a4668853f33152
Mihai Racovit <gratiracovit@gmail.com>
2022-07-28 21:31:26 +0200

    updating tests

--- tests/e2e/notice_publisher_triple_store/test_load_transformed_notice_into_triple_store.py
+++ tests/e2e/notice_publisher_triple_store/test_load_transformed_notice_into_triple_store.py
@@ -3,8 +3,10 @@ from ted_sws.notice_publisher_triple_store.services.load_transformed_notice_into
 from tests.fakes.fake_repository import FakeNoticeRepository
 
 
-SPARQL_QUERY = "select * {?s ?p ?o}"
-Grath = "SELECT * WHERE { { GRAPH ?g { ?s ?p ?o } }"
+SPARQL_QUERY_TRIPLES = "select * {?s ?p ?o}"
+SPARQL_QUERY_GRAPH = "SELECT ?g {  GRAPH ?g { ?s ?p ?o  } }"
+SPARQL_QUERY_FIXED_URI = "select * { <http://data.europa.eu/a4g/resource/ReviewerOrganisationIdentifier/2018-S-175-396207/de2507f9-ae25-37c8-809c-0109efe10669> ?p ?o .} "
+
 
 def test_load_notice_into_triple_store(transformed_complete_notice, allegro_triple_store):
     fake_notice_repo = FakeNoticeRepository()
@@ -15,14 +17,21 @@ def test_load_notice_into_triple_store(transformed_complete_notice, allegro_trip
     sparql_endpoint = allegro_triple_store.get_sparql_triple_store_endpoint(DEFAULT_NOTICE_REPOSITORY_NAME)
     assert sparql_endpoint is not None
 
-    query_result = sparql_endpoint.with_query(sparql_query=SPARQL_QUERY).fetch_tree()
-    assert query_result
-    print(query_result)
-    # assert that the graph in the triple store has more that 1 triple inside
-    if len(SPARQL_QUERY) in query_result >= 1:
+    df_query_result = sparql_endpoint.with_query(sparql_query=SPARQL_QUERY_TRIPLES).fetch_tabular()
+    assert df_query_result is not None
+    # assert that the graph in the triple store has more than 1 triple inside
+    if len(df_query_result) > 0:
         assert True
-    # assert that at least one graph exists in the triple store
-    if SPARQL_QUERY in query_result:
-        assert True
-    # assert that the there is an epo:Identifier object that has the value equal to the notice ID
 
+     # assert that at least one graph exists in the triple store
+    df_query_result = sparql_endpoint.with_query(sparql_query=SPARQL_QUERY_GRAPH).fetch_tabular()
+    assert df_query_result is not None
+    if len(df_query_result) > 0:
+        assert True
+
+     # assert that the there is an epo:SPARQL_QUERY_FIXED_URI object that has the value equal to the notice ID
+    df_query_result = sparql_endpoint.with_query(sparql_query=SPARQL_QUERY_FIXED_URI).fetch_tabular()
+    assert df_query_result is not None
+    print(df_query_result)
+    if len(df_query_result) > 0:
+        assert True

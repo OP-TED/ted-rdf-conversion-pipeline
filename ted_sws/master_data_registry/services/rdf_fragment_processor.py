a7e79157a5c63874f2d15bc8d239df740bbd906d
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-07-29 11:49:18 +0300

    Create rdf_fragment_processor.py

---
+++ ted_sws/master_data_registry/services/rdf_fragment_processor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# rdf_fragment_processor.py
+# Date:  29.07.2022
+# Author: Stratulat Ștefan
+# Email: stefan.stratulat1997@gmail.com
+
+"""
+
+"""
+
+
+def rdf_fragment_extractor(rdf_file_content: str, sparql_):
+    """
+
+    """

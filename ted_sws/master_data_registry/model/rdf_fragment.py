16d4a1917041248faf8f2edae0b6bf990aa95303
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-07-29 11:49:12 +0300

    Create rdf_fragment.py

---
+++ ted_sws/master_data_registry/model/rdf_fragment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# rdf_fragment.py
+# Date:  29.07.2022
+# Author: Stratulat Ștefan
+# Email: stefan.stratulat1997@gmail.com
+
+import rdflib
+
+
+class RDFFragment:
+    """
+
+    """
+
+    rdf_content: str
+    sparql_query: str
+    rdf_fragment_triples: list
+
+
+class RDFFragmentProcessor:
+    """
+
+    """

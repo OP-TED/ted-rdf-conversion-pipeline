7b39eac068978b7819876fd7b53b8b25df4f4f93
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-08-01 16:01:01 +0300

    Update selector_retransform_process_orchestrator.py

--- dags/selector_retransform_process_orchestrator.py
+++ dags/selector_retransform_process_orchestrator.py
@@ -8,8 +8,10 @@ from ted_sws import config
 from ted_sws.core.model.notice import NoticeStatus
 from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
 from ted_sws.event_manager.adapters.event_log_decorator import event_log
+from ted_sws.event_manager.adapters.event_logger import EventLogger
 from ted_sws.event_manager.model.event_message import TechnicalEventMessage, EventMessageMetadata, \
-    EventMessageProcessType
+    EventMessageProcessType, EventMessage
+from ted_sws.event_manager.services.logger_from_context import get_logger_from_dag_context
 
 DAG_NAME = "selector_re_transform_process_orchestrator"
 
@@ -32,10 +34,12 @@ def selector_re_transform_process_orchestrator():
             process_type=EventMessageProcessType.DAG, process_name=DAG_NAME
         ))
     )
-    def select_notices_for_re_transform_and_reset_status():
+    def select_notices_for_re_transform_and_reset_status(**context_args):
+        event_logger: EventLogger = get_logger_from_dag_context(context_args)
         mongodb_client = MongoClient(config.MONGO_DB_AUTH_URL)
         notice_repository = NoticeRepository(mongodb_client=mongodb_client)
         for target_notice_state in RE_TRANSFORM_TARGET_NOTICE_STATES:
+            event_logger.info(event_message=EventMessage(message=f"select notices with status : {target_notice_state}"))
             notices = notice_repository.get_notice_by_status(notice_status=target_notice_state)
             for notice in notices:
                 notice.update_status_to(new_status=NoticeStatus.NORMALISED_METADATA)

81c2a39898ce10de4071d26f0d3a6d036e0ae201
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-08-03 22:20:44 +0300

    Create test_rdf_fragment_processor.py

---
+++ tests/unit/master_data_registry/test_rdf_fragment_processor.py
@@ -0,0 +1,21 @@
+from ted_sws.master_data_registry.services.rdf_fragment_processor import get_rdf_fragment_by_cet_uri_from_string, \
+    get_rdf_fragments_by_cet_uri_from_file
+
+
+def test_get_rdf_fragment_by_cet_uri_from_string(rdf_content, organisation_cet_uri):
+    rdf_fragments = get_rdf_fragment_by_cet_uri_from_string(rdf_file_content=rdf_content, cet_uri=organisation_cet_uri)
+    assert len(rdf_fragments) == 3
+    for rdf_fragment in rdf_fragments:
+        assert rdf_fragment.rdf_content
+        assert rdf_fragment.rdf_fragment_triples
+        assert rdf_fragment.sparql_query
+
+
+
+def test_get_rdf_fragments_by_cet_uri_from_file(rdf_file_path, organisation_cet_uri):
+    rdf_fragments = get_rdf_fragments_by_cet_uri_from_file(rdf_file_path=rdf_file_path, cet_uri=organisation_cet_uri)
+    assert len(rdf_fragments) == 3
+    for rdf_fragment in rdf_fragments:
+        assert rdf_fragment.rdf_content
+        assert rdf_fragment.rdf_fragment_triples
+        assert rdf_fragment.sparql_query

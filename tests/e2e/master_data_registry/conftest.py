e552f53b9f68131732d831cf54b8380eac523e59
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-08-03 22:17:09 +0300

    Update conftest.py

--- tests/e2e/master_data_registry/conftest.py
+++ tests/e2e/master_data_registry/conftest.py
@@ -1,33 +1,27 @@
+import pathlib
 from string import Template
 
 import pytest
 
+from ted_sws import config
+from tests import TEST_DATA_PATH
+
 
 @pytest.fixture
 def triple_store_endpoint_url() -> str:
-    return "https://agraph.staging.ted-data.eu/repositories/package_f03"
+    return config.TRIPLE_STORE_ENDPOINT_URL
 
 
 @pytest.fixture
-def get_all_organisation_sparql_query() -> str:
-    return """
-prefix org: <http://www.w3.org/ns/org#>
-prefix epo: <http://data.europa.eu/a4g/ontology#>
-SELECT ?s
-{
-  ?s a epo:Organisation .
-}
-    """
+def rdf_file_path() -> pathlib.Path:
+    return TEST_DATA_PATH / "example.ttl"
 
 
 @pytest.fixture
-def get_all_triples_by_uri_sparql_query() -> Template:
-    return Template("""
-prefix org: <http://www.w3.org/ns/org#>
-prefix epo: <http://data.europa.eu/a4g/ontology#>
-select ?s ?p ?o 
-{
-  values ?s {<$uri>}
-  ?s ?p ?o .                                                                                                              
-}
-    """)
+def rdf_content(rdf_file_path) -> str:
+    return rdf_file_path.read_text(encoding="utf-8")
+
+
+@pytest.fixture
+def organisation_cet_uri() -> str:
+    return "http://data.europa.eu/a4g/ontology#Organisation"

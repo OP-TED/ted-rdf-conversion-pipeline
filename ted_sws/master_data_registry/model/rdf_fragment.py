580f6ccad6413d20e7bbb406c003083f98662e2e
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-08-03 22:16:19 +0300

    Update rdf_fragment.py

--- ted_sws/master_data_registry/model/rdf_fragment.py
+++ ted_sws/master_data_registry/model/rdf_fragment.py
@@ -4,17 +4,19 @@
 # Date:  29.07.2022
 # Author: Stratulat Ștefan
 # Email: stefan.stratulat1997@gmail.com
+from typing import List, Tuple
 
-import rdflib
+from rdflib.term import URIRef
+
+from ted_sws.core.model import PropertyBaseModel
 
 
-class RDFFragment:
+class RDFFragment(PropertyBaseModel):
     """
-
+        This class is used to store an RDFFragment.
     """
-
     rdf_content: str
     sparql_query: str
-    rdf_fragment_triples: list
+    rdf_fragment_triples: List[Tuple[URIRef, URIRef, URIRef]]
 
 

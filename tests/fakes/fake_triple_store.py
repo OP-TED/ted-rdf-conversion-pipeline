12bfb9c199a96498dfefa302fa4de6353451e8a7
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-08-03 22:26:16 +0300

    Update fake_triple_store.py

--- tests/fakes/fake_triple_store.py
+++ tests/fakes/fake_triple_store.py
@@ -1,9 +1,15 @@
+from typing import List, Tuple
+
 import pandas as pd
+from rdflib import URIRef
 
 from ted_sws.data_manager.adapters.sparql_endpoint import TripleStoreEndpointABC
 
 
 class FakeTripleStoreEndpoint(TripleStoreEndpointABC):
+    def fetch_rdf(self) -> List[Tuple[URIRef, URIRef, URIRef]]:
+        return []
+
     def with_query(self, sparql_query: str, substitution_variables: dict = None,
                    sparql_prefixes: str = "") -> 'TripleStoreEndpointABC':
         return self

72f63a89ed097034e21372b5085532a0a122eba4
Mihai Racovit <gratiracovit@gmail.com>
2022-08-09 23:35:55 +0200

    updated tests

--- tests/e2e/notice_publisher_triple_store/test_load_transformed_notice_into_triple_store.py
+++ tests/e2e/notice_publisher_triple_store/test_load_transformed_notice_into_triple_store.py
@@ -20,7 +20,7 @@ def test_load_notice_into_triple_store(transformed_complete_notice, allegro_trip
     assert df_query_result is not None
     assert len(df_query_result) > 0
 
-    df_query_result = sparql_endpoint.with_query(sparql_query=SPARQL_QUERY_GRAPH).fetch_tabular()
+    df_query_result = sparql_endpoint.with_query(sparql_query=SPARQL_QUERY_GRAPH).fetch_tree()
     assert df_query_result is not None
     assert len(df_query_result) > 0
 

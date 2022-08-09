2f53263874d92d99793f3865ef8012b9694ad4fb
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-08-09 18:01:08 +0300

    Update test_fuseki_triple_store.py

--- tests/e2e/data_manager/test_fuseki_triple_store.py
+++ tests/e2e/data_manager/test_fuseki_triple_store.py
@@ -44,5 +44,4 @@ def test_fuseki_triple_store_get_sparql_endpoint(fuseki_triple_store):
     assert sparql_endpoint is not None
     df_query_result = sparql_endpoint.with_query(sparql_query=SPARQL_QUERY_TRIPLES).fetch_tabular()
     assert df_query_result is not None
-    if len(df_query_result) > 0:
-        assert True
+    assert len(df_query_result) > 0

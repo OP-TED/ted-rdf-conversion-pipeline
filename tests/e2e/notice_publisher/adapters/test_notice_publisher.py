cc3ce24e7e34fe6ad5dbc5425dc3e8e8109fae1f
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-08-15 14:49:18 +0300

    Update test_notice_publisher.py

--- tests/e2e/notice_publisher/adapters/test_notice_publisher.py
+++ tests/e2e/notice_publisher/adapters/test_notice_publisher.py
@@ -3,30 +3,16 @@ import tempfile
 import pytest
 
 from ted_sws import config
-from ted_sws.notice_publisher.adapters.notice_publisher import NoticePublisherFactory, Publisher
-from ted_sws.notice_publisher.adapters.sftp_notice_publisher import SFTPNoticePublisher
-
-
-def test_notice_publisher():
-    with pytest.raises(Exception):
-        NoticePublisherFactory.get_publisher(publisher=Publisher.SFTP, hostname=None)
-    NoticePublisherFactory.get_publisher(publisher=Publisher.SFTP, hostname="HOST", username="USER", password="PASS",
-                                         port=0, remote_path="")
+from ted_sws.notice_publisher.adapters.sftp_notice_publisher import SFTPPublisher
 
 
 def test_sftp_notice_publisher():
-    user = config.SFTP_USER
-    password = config.SFTP_PASSWORD
-    host = None
-    port = 0
-
-    sftp_publisher = SFTPNoticePublisher(hostname=host, username=user, password=password, port=port, remote_path=None)
+    sftp_publisher = SFTPPublisher(port=0)
 
     with pytest.raises(Exception):
         sftp_publisher.connect()
 
-    sftp_publisher.hostname = config.SFTP_HOST
-    sftp_publisher.port = int(config.SFTP_PORT)
+    sftp_publisher.port = config.SFTP_PORT
     sftp_publisher.connect()
 
     source_file = tempfile.NamedTemporaryFile()
@@ -51,3 +37,4 @@ def test_sftp_notice_publisher():
     assert not sftp_publisher.connection.exists(remote_path)
 
     sftp_publisher.disconnect()
+

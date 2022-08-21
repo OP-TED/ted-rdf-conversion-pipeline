0e0f7e222003c9b733005615a38f5ede3504c15c
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-08-22 00:56:04 +0300

    Update __init__.py

--- tests/old_features/__init__.py
+++ tests/old_features/__init__.py
@@ -8,12 +8,4 @@
 """ """
 
 
-def str2bool(value: str) -> bool:
-    """
-        Parse a string value and cast it into its boolean value
-    :param value:
-    :return:
-    """
-    if value in ["y", "yes", "t", "true", "on", "1"]: return True
-    if value in ["n", "no", "f", "false", "off", "0"]: return False
-    raise ValueError("boolean value unrecognised")
+

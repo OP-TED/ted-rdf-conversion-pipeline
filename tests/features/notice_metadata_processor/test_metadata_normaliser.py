e96e200df616d05146f23726d668f6cf06cc82dd
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-08-22 00:55:39 +0300

    Create test_metadata_normaliser.py

---
+++ tests/features/notice_metadata_processor/test_metadata_normaliser.py
@@ -0,0 +1,36 @@
+from pytest_bdd import scenario, given, when, then, parsers
+
+from ted_sws.core.model.notice import NoticeStatus
+from ted_sws.notice_metadata_processor.services.metadata_normalizer import MetadataNormaliser
+
+
+@scenario('metadata_normaliser.feature', 'Normalizing a notice metadata')
+def test_extract_metadata():
+    """normalising metadata"""
+
+
+@given("a notice", target_fixture="notice")
+def step_impl(f03_notice_2020):
+    return f03_notice_2020
+
+
+@when("the normalize process is executed")
+def step_impl(notice):
+    MetadataNormaliser(notice=notice).normalise_metadata()
+
+
+@then(parsers.parse("a normalized notice {metadata} is {possibly} available"))
+def step_impl(notice, metadata, possibly):
+    metadata_value = notice.normalised_metadata.dict()[metadata]
+    is_value_there = "True" if metadata_value else "False"
+    assert is_value_there == possibly
+
+
+@then("the notice status is NORMALISED_METADATA")
+def step_impl(notice):
+    assert notice.status is NoticeStatus.NORMALISED_METADATA
+
+
+@then("normalised metadata is available")
+def step_impl(notice):
+    assert notice.normalised_metadata

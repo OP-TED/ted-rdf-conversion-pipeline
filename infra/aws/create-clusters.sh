1ad3b7f7485f92386e82f4dfdcad877c515edbe4
Dragos0000 <dragos.paun0000@gmail.com>
2022-08-24 17:10:37 +0100

    aws scripts

---
+++ infra/aws/create-clusters.sh
@@ -0,0 +1,26 @@
+source .env
+export $(cat .env | xargs)
+CLUSTERS=(mongo digest-api airflow fuseki metabase mongo-express)
+
+create_cluster_config_and_profiles() {
+  ecs-cli configure --cluster $1-cluster --default-launch-type EC2 --config-name $1-cluster --region $REGION
+  ecs-cli configure profile --access-key $ACCESS_KEY --secret-key $SECRET_KEY --profile-name $1-cluster-profile
+}
+
+for CLUSTER in "${CLUSTERS[@]}"
+do
+  create_cluster_config_and_profiles $CLUSTER
+done
+
+create_cluster() {
+  echo "VPC ID is $VPC_ID"
+  echo "Creating cluster for $1"
+  ecs-cli up --force --instance-role ecsInstanceRole --extra-user-data botocore.sh --keypair tedSWS --size 1 --instance-type $1 --cluster-config $2 --ecs-profile $3 --vpc $VPC_ID --subnets $SUBNET_1_ID,$SUBNET_2_ID,$SUBNET_3_ID --security-group $SECURITY_GROUP
+}
+
+create_cluster $MONGO_INSTANCE_TYPE mongo-cluster mongo-cluster-profile
+create_cluster $DIGEST_API_INSTANCE_TYPE digest-api-cluster digest-api-cluster-profile
+create_cluster $AIRFLOW_INSTANCE_TYPE airflow-cluster airflow-cluster-profile
+create_cluster $FUSEKI_INSTANCE_TYPE fuseki-cluster fuseki-cluster-profile
+create_cluster $METABASE_INSTANCE_TYPE metabase-cluster metabase-cluster-profile
+create_cluster $MONGO_EXPRESS_INSTANCE_TYPE mongo-express-cluster mongo-express-cluster-profile
---
+++ infra/aws/create-services.sh
@@ -0,0 +1,26 @@
+source .env
+export $(cat .env | xargs)
+
+SERVICES_WITHOUT_DNS=(airflow metabase mongo-express)
+SERVICES_WITH_DNS_CREATION=(mongo digest-api fuseki)
+
+create_service_with_service_discovery(){
+  ecs-cli compose --project-name $1-service --file $1.yml --ecs-params $1-ecs-params.yml --debug service create --region $REGION --ecs-profile $1-cluster-profile --cluster-config $1-cluster --private-dns-namespace ted_sws --vpc $VPC_ID --dns-type A --enable-service-discovery
+}
+
+create_service_without_service_discovery(){
+  ecs-cli compose --project-name $1-service --file $1.yml --ecs-params $1-ecs-params.yml --debug service create --region $REGION --ecs-profile $1-cluster-profile --cluster-config $1-cluster
+}
+
+
+for SERVICE in "${SERVICES_WITH_DNS_CREATION[@]}"
+do
+  echo "Starting $SERVICE service"
+  create_service_with_service_discovery $SERVICE
+done
+
+for SERVICE in "${SERVICES_WITHOUT_DNS[@]}"
+do
+  echo "Starting $SERVICE service"
+  create_service_without_service_discovery $SERVICE
+done

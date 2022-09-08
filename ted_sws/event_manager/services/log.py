0040c3c9df44c836c6b081ff2ff3fd521a79e851
Kolea Plesco <kalean.bl@gmail.com>
2022-09-08 22:44:29 +0300

    Updated log services

--- ted_sws/event_manager/services/log.py
+++ ted_sws/event_manager/services/log.py
@@ -1,4 +1,5 @@
-from ted_sws.event_manager.model.event_message import EventMessage
+from ted_sws.event_manager.model.event_message import EventMessage, TechnicalEventMessage, NoticeEventMessage, \
+    MappingSuiteEventMessage
 from ted_sws.event_manager.services.logger_from_context import get_logger
 
 
@@ -12,3 +13,56 @@ def log_error(message: str, name: str = None):
 
 def log_debug(message: str, name: str = None):
     get_logger(name=name).debug(event_message=EventMessage(message=message))
+
+
+def log_warning(message: str, name: str = None):
+    get_logger(name=name).warning(event_message=EventMessage(message=message))
+
+
+def log_technical_info(message: str, name: str = None):
+    get_logger(name=name).info(event_message=TechnicalEventMessage(message=message))
+
+
+def log_technical_error(message: str, name: str = None):
+    get_logger(name=name).error(event_message=TechnicalEventMessage(message=message))
+
+
+def log_technical_debug(message: str, name: str = None):
+    get_logger(name=name).debug(event_message=TechnicalEventMessage(message=message))
+
+
+def log_technical_warning(message: str, name: str = None):
+    get_logger(name=name).warning(event_message=TechnicalEventMessage(message=message))
+
+
+def log_notice_info(message: str, name: str = None):
+    get_logger(name=name).info(event_message=NoticeEventMessage(message=message))
+
+
+def log_notice_error(message: str, name: str = None):
+    get_logger(name=name).error(event_message=NoticeEventMessage(message=message))
+
+
+def log_notice_debug(message: str, name: str = None):
+    get_logger(name=name).debug(event_message=NoticeEventMessage(message=message))
+
+
+def log_notice_warning(message: str, name: str = None):
+    get_logger(name=name).warning(event_message=NoticeEventMessage(message=message))
+
+
+def log_mapping_suite_info(message: str, name: str = None):
+    get_logger(name=name).info(event_message=MappingSuiteEventMessage(message=message))
+
+
+def log_mapping_suite_error(message: str, name: str = None):
+    get_logger(name=name).error(event_message=MappingSuiteEventMessage(message=message))
+
+
+def log_mapping_suite_debug(message: str, name: str = None):
+    get_logger(name=name).debug(event_message=MappingSuiteEventMessage(message=message))
+
+
+def log_mapping_suite_warning(message: str, name: str = None):
+    get_logger(name=name).warning(event_message=MappingSuiteEventMessage(message=message))
+

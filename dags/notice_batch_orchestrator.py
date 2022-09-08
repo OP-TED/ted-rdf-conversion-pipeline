d019563ec4df34889ac0e6316f1c4771ec044550
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-09-08 23:18:13 +0300

    Update notice_batch_orchestrator.py

--- dags/notice_batch_orchestrator.py
+++ dags/notice_batch_orchestrator.py
@@ -1,4 +1,5 @@
 from itertools import chain, islice
+from typing import Iterator
 
 from airflow.decorators import dag, task
 from airflow.operators.python import get_current_context
@@ -9,7 +10,7 @@ from dags import DEFAULT_DAG_ARGUMENTS
 from dags.notice_batch_worker import NOTICE_BATCH_KEY
 from ted_sws import config
 from ted_sws.core.model.notice import NoticeStatus
-from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
+from ted_sws.data_manager.adapters.notice_repository import NoticeRepository, NOTICE_STATUS
 from ted_sws.event_manager.adapters.event_log_decorator import event_log
 from ted_sws.event_manager.model.event_message import TechnicalEventMessage, EventMessageMetadata, \
     EventMessageProcessType
@@ -26,6 +27,11 @@ def chunks(iterable, chunk_size: int):
     iterator = iter(iterable)
     for first in iterator:
         yield chain([first], islice(iterator, chunk_size - 1))
+
+
+def get_notice_ids(notice_repository: NoticeRepository, notice_status: str) -> Iterator[str]:
+    for result_dict in notice_repository.collection.find({NOTICE_STATUS: notice_status}, {"ted_id": 1}):
+        yield result_dict["ted_id"]
 
 
 @dag(default_args=DEFAULT_DAG_ARGUMENTS, schedule_interval=None, tags=['master', 'notice_batch_orchestrator'])
@@ -49,12 +55,13 @@ def notice_batch_orchestrator():
         mongodb_client = MongoClient(config.MONGO_DB_AUTH_URL)
         notice_repository = NoticeRepository(mongodb_client=mongodb_client)
         notice_batch_counter = 0
-        for notice_batch in chunks(notice_repository.get_notice_by_status(notice_status=NoticeStatus[notice_status]),
+        for notice_batch in chunks(get_notice_ids(notice_repository=notice_repository,
+                                                  notice_status=notice_status),
                                    chunk_size=notice_batch_size):
             TriggerDagRunOperator(
                 task_id=f'trigger_notice_batch_worker_dag_{notice_batch_counter}',
                 trigger_dag_id="notice_batch_worker",
-                conf={NOTICE_BATCH_KEY: list(notice_batch)}
+                conf={NOTICE_BATCH_KEY: [notice.ted_id for notice in notice_batch]}
             ).execute(context=context)
             notice_batch_counter += 1
 

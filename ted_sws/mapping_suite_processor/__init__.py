790212f3d8a8c89c345a97f12f337a688ea8cf48
Kolea Plesco <kalean.bl@gmail.com>
2022-09-16 15:46:43 +0300

    Updated tests

--- ted_sws/mapping_suite_processor/__init__.py
+++ ted_sws/mapping_suite_processor/__init__.py
@@ -4,4 +4,3 @@ RULES_E_FORM_BT_NAME = 'eForm BT Name (Provisional/Indicative) (O)'
 RULES_FIELD_XPATH = 'Field XPath (M)'
 RULES_SF_FIELD_ID = 'Standard Form Field ID (M)'
 RULES_E_FORM_BT_ID = 'eForm BT-ID (Provisional/Indicative) (O)'
-

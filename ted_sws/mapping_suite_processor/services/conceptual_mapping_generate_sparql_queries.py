c20c818b884efba23cd0894fda0391a8d3ae18a2
Dumitru <d.prijilevschi@gmail.com>
2022-09-26 18:08:26 +0300

    Update conceptual_mapping_generate_sparql_queries.py

--- ted_sws/mapping_suite_processor/services/conceptual_mapping_generate_sparql_queries.py
+++ ted_sws/mapping_suite_processor/services/conceptual_mapping_generate_sparql_queries.py
@@ -15,6 +15,8 @@ RULES_PROPERTY_PATH = 'Property path (M)'
 
 CL_FIELD_VALUE = 'Field Value (in XML)'
 CL_MAPPING_REFERENCE = 'Mapping Reference (in ePO)'
+CL_SUPER_TYPE = 'SuperType'
+CL_XPATH_FRAGMENT = 'XML PATH Fragment'
 
 DEFAULT_RQ_NAME = 'sparql_query_'
 
@@ -24,6 +26,7 @@ SPARQL_PREFIX_LINE = 'PREFIX {prefix}: <{value}>'
 
 def get_sparql_prefixes(sparql_q: str) -> set:
     finds: list = re.findall(SPARQL_PREFIX_PATTERN, sparql_q)
+    finds.append('rdf')
     return set(finds)
 
 
@@ -34,42 +37,55 @@ def concat_field_xpath(base_xpath: str, field_xpath: str, separator: str = ", ")
     return separator.join([base_xpath + xpath for xpath in field_xpath.splitlines()])
 
 
-def get_elem_reference(class_value: str, cl_dfs: dict, field_xpath: str) -> str:
-
+def _get_elem_reference(class_value: str, cl_dfs: dict, field_xpath: list) -> str:
     if '(from ' in class_value:
-        class_name = class_value.split()[0].split(':')[-1].lower()
+
+        # Find CL sheet
         cl_id = class_value.split()[-1][:-1]
-        cl_sheet = pd.DataFrame()
+        cl_sheet: pd.DataFrame() = pd.DataFrame()
         for sheet_name in cl_dfs:
             if sheet_name.startswith(cl_id):
                 cl_sheet = cl_dfs[sheet_name]
-        for index, row in cl_sheet.iterrows():
-            tmp_row = row[CL_FIELD_VALUE]
-            if class_name == tmp_row.split()[0] and field_xpath == tmp_row.split()[-1]:
-                return row[CL_MAPPING_REFERENCE]
+
+        # Find elem type
+        if not cl_sheet.empty:
+            class_value = class_value.split()[0]
+            for index, row in cl_sheet.iterrows():
+                class_super_type = row[CL_SUPER_TYPE]
+                xpath_fragment = row[CL_XPATH_FRAGMENT]
+                for field_xpath_fragment in reversed(field_xpath):
+                    if class_value == class_super_type and field_xpath_fragment == xpath_fragment:
+                        return row[CL_MAPPING_REFERENCE]
     else:
-        return class_value.split()[0]
+        return class_value
 
     return ''
 
 
-def generate_subject_type(class_path: str, cl_dfs: dict, field_xpath: str) -> str:
-    subject_reference = get_elem_reference(class_path.split(' / ')[0], cl_dfs,
-                                           field_xpath.split('/')[0].lower() if not pd.isna(field_xpath) else '')
+def _generate_subject_type(class_path: str, cl_dfs: dict, field_xpath: str) -> str:
+    subject_reference = _get_elem_reference(class_path.split(' / ')[0], cl_dfs,
+                                            field_xpath.split('/') if not pd.isna(field_xpath) else '')
     return f"?this rdf:type {subject_reference} ." if subject_reference else ''
 
 
-def generate_object_type(class_path: str, cl_dfs: dict, field_xpath: str) -> str:
-    object_reference = get_elem_reference(class_path.split(' / ')[-1], cl_dfs,
-                                          field_xpath.split('/')[-1].lower() if not pd.isna(field_xpath) else '')
+def _generate_object_type(class_path: str, cl_dfs: dict, field_xpath: str) -> str:
+
+    # Temporary solution
+    class_path = class_path.split(' / ')[-1]
+    if 'at-voc:' in class_path:
+        return ''
+
+    object_reference = _get_elem_reference(class_path, cl_dfs,
+                                           field_xpath.split('/') if not pd.isna(field_xpath) else '')
     return f"?value rdf:type {object_reference} ." if object_reference else ''
 
 
-def sparql_validation_generator(data: pd.DataFrame, base_xpath: str, cl_dfs: dict) -> Iterator[str]:
+def sparql_validation_generator(data: pd.DataFrame, base_xpath: str, controlled_list_dfs: dict) -> Iterator[str]:
     """
         This function generates SPARQL queries based on data in the dataframe.
     :param data:
     :param base_xpath:
+    :param controlled_list_dfs:
     :return:
     """
 
@@ -84,6 +100,12 @@ def sparql_validation_generator(data: pd.DataFrame, base_xpath: str, cl_dfs: dic
         prefixes = [SPARQL_PREFIX_LINE.format(
             prefix=prefix, value=PREFIXES_DEFINITIONS.get(prefix)
         ) for prefix in get_sparql_prefixes(property_path)]
+
+        subject_type = _generate_subject_type(class_path, controlled_list_dfs, field_xpath) \
+            if '?this' in property_path else ''
+        object_type = _generate_object_type(class_path, controlled_list_dfs, field_xpath) \
+            if '?value' in property_path else ''
+
         yield f"#title: {sf_field_id} - {sf_field_name}\n" \
               f"#description: “{sf_field_id} - {sf_field_name}” in SF corresponds to “{e_form_bt_id} " \
               f"{e_form_bt_name}” in eForms. The corresponding XML element is " \
@@ -92,9 +114,15 @@ def sparql_validation_generator(data: pd.DataFrame, base_xpath: str, cl_dfs: dic
               f"#xpath: {concat_field_xpath(base_xpath, field_xpath, separator=',')}" \
               "\n" + "\n" + "\n".join(prefixes) + "\n\n" \
                                                   f"ASK WHERE {{ \n\t\t" \
-                                                  f"{generate_subject_type(class_path, cl_dfs, field_xpath)}\n\t\t" \
-                                                  f"{generate_object_type(class_path, cl_dfs, field_xpath)}\n\t\t" \
+                                                  f"{subject_type}\n\t\t" \
+                                                  f"{object_type}\n\t\t" \
                                                   f"{property_path} }}"
+
+
+def _process_concept_mapping_sheet(sheet: pd.DataFrame) -> pd.DataFrame:
+    sheet.columns = sheet.iloc[0]
+    sheet = sheet[1:]
+    return sheet
 
 
 def mapping_suite_processor_generate_sparql_queries(conceptual_mappings_file_path: pathlib.Path,
@@ -107,24 +135,20 @@ def mapping_suite_processor_generate_sparql_queries(conceptual_mappings_file_pat
     with open(conceptual_mappings_file_path, 'rb') as excel_file:
         conceptual_mappings_df = pd.read_excel(excel_file, sheet_name=None)
 
-        cl_dfs = {}
-        for SHEET_NAMES in conceptual_mappings_df:
-            if SHEET_NAMES.startswith('CL'):
-                conceptual_mappings_df[SHEET_NAMES].columns = conceptual_mappings_df[SHEET_NAMES].iloc[0]
-                conceptual_mappings_df[SHEET_NAMES] = conceptual_mappings_df[SHEET_NAMES][1:]
-                cl_dfs[SHEET_NAMES] = conceptual_mappings_df[SHEET_NAMES][
-                    conceptual_mappings_df[SHEET_NAMES][[CL_FIELD_VALUE, CL_MAPPING_REFERENCE]].notnull()]
+        controlled_list_dfs = {}
+        for sheet_name in conceptual_mappings_df:
+            if sheet_name.startswith('CL'):
+                controlled_list_dfs[sheet_name] = _process_concept_mapping_sheet(conceptual_mappings_df[sheet_name])
 
-        conceptual_mappings_rules_df = conceptual_mappings_df[CONCEPTUAL_MAPPINGS_RULES_SHEET_NAME]
-        conceptual_mappings_rules_df.columns = conceptual_mappings_rules_df.iloc[0]
-        conceptual_mappings_rules_df = conceptual_mappings_rules_df[1:]
+        conceptual_mappings_rules_df = _process_concept_mapping_sheet(
+            conceptual_mappings_df[CONCEPTUAL_MAPPINGS_RULES_SHEET_NAME])
         conceptual_mappings_rules_df = conceptual_mappings_rules_df[
             conceptual_mappings_rules_df[RULES_PROPERTY_PATH].notnull()]
-        metadata_df = conceptual_mappings_df[CONCEPTUAL_MAPPINGS_METADATA_SHEET_NAME] #pd.read_excel(excel_file, sheet_name=CONCEPTUAL_MAPPINGS_METADATA_SHEET_NAME)
+
+        metadata_df = conceptual_mappings_df[CONCEPTUAL_MAPPINGS_METADATA_SHEET_NAME]
         metadata = metadata_df.set_index('Field').T.to_dict('list')
         base_xpath = metadata[BASE_XPATH_FIELD][0]
-
-    sparql_queries = sparql_validation_generator(conceptual_mappings_rules_df, base_xpath, cl_dfs)
+    sparql_queries = sparql_validation_generator(conceptual_mappings_rules_df, base_xpath, controlled_list_dfs)
 
     output_sparql_queries_folder_path.mkdir(parents=True, exist_ok=True)
     for index, sparql_query in enumerate(sparql_queries):

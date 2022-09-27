3953f97e0af355fe05743442e88a730eb49270da
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-09-27 18:03:57 +0300

    add validation DAG

--- ted_sws/notice_validator/services/check_availability_of_notice_in_cellar.py
+++ ted_sws/notice_validator/services/check_availability_of_notice_in_cellar.py
@@ -1,3 +1,4 @@
+from ted_sws.core.model.notice import Notice, NoticeStatus
 from ted_sws.data_manager.adapters.sparql_endpoint import SPARQLTripleStoreEndpoint
 
 WEBAPI_SPARQL_URL = "https://publications.europa.eu/webapi/rdf/sparql"
@@ -11,3 +12,18 @@ def check_availability_of_notice_in_cellar(notice_uri: str, endpoint_url: str = 
         endpoint_url=endpoint_url,
         use_env_credentials=False).with_query(sparql_query=query).fetch_tree()
     return result['boolean']
+
+
+def generate_notice_uri_from_notice_id(notice_id: str) -> str:
+    # TODO: implement notice_uri logic
+    raise NotImplementedError
+
+
+def validate_notice_availability_in_cellar(notice: Notice) -> Notice:
+    if notice.status == NoticeStatus.PUBLISHED:
+        notice_uri = generate_notice_uri_from_notice_id(notice_id=notice.ted_id)
+        if check_availability_of_notice_in_cellar(notice_uri=notice_uri):
+            notice.update_status_to(new_status=NoticeStatus.PUBLICLY_AVAILABLE)
+        else:
+            notice.update_status_to(new_status=NoticeStatus.PUBLICLY_UNAVAILABLE)
+    return notice

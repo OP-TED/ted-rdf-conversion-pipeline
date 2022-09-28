fa75aada1769f3dc0a3cf889777005ad284ac617
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-09-28 18:22:24 +0300

    add new params for selectors DAGs

--- dags/selector_retransform_process_orchestrator.py
+++ dags/selector_retransform_process_orchestrator.py
@@ -1,8 +1,10 @@
 from airflow.decorators import dag, task
 
 from dags import DEFAULT_DAG_ARGUMENTS
-from dags.dags_utils import push_dag_downstream
-from dags.operators.DagBatchPipelineOperator import NOTICE_IDS_KEY, TriggerNoticeBatchPipelineOperator
+from dags.dags_utils import push_dag_downstream, get_dag_param
+from dags.notice_process_workflow import NOTICE_TRANSFORMATION_PIPELINE_TASK_ID
+from dags.operators.DagBatchPipelineOperator import NOTICE_IDS_KEY, TriggerNoticeBatchPipelineOperator, \
+    EXECUTE_ONLY_ONE_STEP_KEY
 from dags.pipelines.notice_selectors_pipelines import notice_ids_selector_by_status
 from ted_sws.core.model.notice import NoticeStatus
 from ted_sws.event_manager.adapters.event_log_decorator import event_log
@@ -11,13 +13,12 @@ from ted_sws.event_manager.model.event_message import TechnicalEventMessage, Eve
 
 DAG_NAME = "selector_re_transform_process_orchestrator"
 
-RE_TRANSFORM_TARGET_NOTICE_STATES = [NoticeStatus.ELIGIBLE_FOR_TRANSFORMATION,
-                                     NoticeStatus.PREPROCESSED_FOR_TRANSFORMATION,
-                                     NoticeStatus.INELIGIBLE_FOR_TRANSFORMATION, NoticeStatus.TRANSFORMED,
-                                     NoticeStatus.DISTILLED, NoticeStatus.VALIDATED,
-                                     NoticeStatus.INELIGIBLE_FOR_PACKAGING
-                                     ]
+RE_TRANSFORM_TARGET_NOTICE_STATES = [NoticeStatus.NORMALISED_METADATA, NoticeStatus.INELIGIBLE_FOR_TRANSFORMATION]
 TRIGGER_NOTICE_PROCESS_WORKFLOW_TASK_ID = "trigger_notice_process_workflow"
+FORM_NUMBER_DAG_PARAM = "form_number"
+START_DATE_DAG_PARAM = "start_date"
+END_DATE_DAG_PARAM = "end_date"
+XSD_VERSION_DAG_PARAM = "xsd_version"
 
 
 @dag(default_args=DEFAULT_DAG_ARGUMENTS,
@@ -32,11 +33,20 @@ def selector_re_transform_process_orchestrator():
         ))
     )
     def select_notices_for_re_transform():
-        notice_ids = notice_ids_selector_by_status(notice_statuses=RE_TRANSFORM_TARGET_NOTICE_STATES)
+        form_number = get_dag_param(key=FORM_NUMBER_DAG_PARAM)
+        start_date = get_dag_param(key=START_DATE_DAG_PARAM)
+        end_date = get_dag_param(key=END_DATE_DAG_PARAM)
+        xsd_version = get_dag_param(key=XSD_VERSION_DAG_PARAM)
+        notice_ids = notice_ids_selector_by_status(notice_statuses=RE_TRANSFORM_TARGET_NOTICE_STATES,
+                                                   form_number=form_number, start_date=start_date,
+                                                   end_date=end_date, xsd_version=xsd_version)
         push_dag_downstream(key=NOTICE_IDS_KEY, value=notice_ids)
 
     trigger_notice_process_workflow = TriggerNoticeBatchPipelineOperator(
-        task_id=TRIGGER_NOTICE_PROCESS_WORKFLOW_TASK_ID)
+        task_id=TRIGGER_NOTICE_PROCESS_WORKFLOW_TASK_ID,
+        execute_only_one_step=get_dag_param(key=EXECUTE_ONLY_ONE_STEP_KEY),
+        start_with_step_name=NOTICE_TRANSFORMATION_PIPELINE_TASK_ID
+    )
     select_notices_for_re_transform() >> trigger_notice_process_workflow
 
 

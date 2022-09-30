db94bbbd85bc60c6fac88bf19aebed26b0edddcd
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-09-30 13:09:37 +0300

    Update test_notice_processor_pipelines.py

--- tests/e2e/dags/pipelines/test_notice_processor_pipelines.py
+++ tests/e2e/dags/pipelines/test_notice_processor_pipelines.py
@@ -1,4 +1,6 @@
-from dags.pipelines.notice_processor_pipelines import notice_normalisation_pipeline, notice_transformation_pipeline
+from dags.pipelines.notice_processor_pipelines import notice_normalisation_pipeline, notice_transformation_pipeline, \
+    notice_validation_pipeline, notice_package_pipeline, notice_publish_pipeline
+from ted_sws.core.model.notice import NoticeStatus
 from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
 from ted_sws.mapping_suite_processor.services.conceptual_mapping_processor import \
     mapping_suite_processor_from_github_expand_and_load_package_in_mongo_db
@@ -13,24 +15,18 @@ def test_notice_processor_pipelines(fake_mongodb_client):
         mongodb_client=fake_mongodb_client,
         load_test_data=True
     )
-
     notice_id = notice_ids[0]
     notice_repository = NoticeRepository(mongodb_client=fake_mongodb_client)
     notice = notice_repository.get(reference=notice_id)
-
-    normalisation_pipeline_output = notice_normalisation_pipeline(notice=notice,
-                                                                  mongodb_client=fake_mongodb_client)
-    assert normalisation_pipeline_output.processed
-    assert normalisation_pipeline_output.store_result
-    assert normalisation_pipeline_output.notice
-    normalised_notice = normalisation_pipeline_output.notice
-
-    transformation_pipeline_output = notice_transformation_pipeline(notice=normalised_notice,
-                                                                    mongodb_client=fake_mongodb_client)
-    assert transformation_pipeline_output.processed
-    assert transformation_pipeline_output.store_result
-    assert transformation_pipeline_output.notice
-    transformed_notice = transformation_pipeline_output.notice
-
-    print(transformed_notice.normalised_metadata.form_number)
-    print(transformed_notice.status)
+    pipelines = [notice_normalisation_pipeline, notice_transformation_pipeline, notice_validation_pipeline,
+                 notice_package_pipeline, notice_publish_pipeline]
+    notice_states = [NoticeStatus.RAW, NoticeStatus.NORMALISED_METADATA, NoticeStatus.DISTILLED,
+                     NoticeStatus.VALIDATED, NoticeStatus.PACKAGED, NoticeStatus.PUBLISHED]
+    for index, pipeline in enumerate(pipelines):
+        assert notice.status == notice_states[index]
+        pipeline_output = pipeline(notice=notice, mongodb_client=fake_mongodb_client)
+        assert pipeline_output.processed, f"{pipeline.__name__} not processed!"
+        assert pipeline_output.store_result
+        assert pipeline_output.notice
+        notice = pipeline_output.notice
+        assert notice.status == notice_states[index + 1]

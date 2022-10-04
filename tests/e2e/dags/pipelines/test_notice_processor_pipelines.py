9e208bef10a60b5156882a26ebdc95e78672c8a4
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-10-04 12:09:17 +0300

    Update test_notice_processor_pipelines.py

--- tests/e2e/dags/pipelines/test_notice_processor_pipelines.py
+++ tests/e2e/dags/pipelines/test_notice_processor_pipelines.py
@@ -7,15 +7,16 @@ from ted_sws.mapping_suite_processor.services.conceptual_mapping_processor impor
 
 MAPPING_SUITE_PACKAGE_NAME = "package_F03_test"
 MAPPING_SUITE_PACKAGE_ID = f"{MAPPING_SUITE_PACKAGE_NAME}_v2.3.0"
+NOTICE_ID = "057215-2021"
 
 
 def test_notice_processor_pipelines(fake_mongodb_client):
-    notice_ids = mapping_suite_processor_from_github_expand_and_load_package_in_mongo_db(
+    mapping_suite_processor_from_github_expand_and_load_package_in_mongo_db(
         mapping_suite_package_name=MAPPING_SUITE_PACKAGE_NAME,
         mongodb_client=fake_mongodb_client,
         load_test_data=True
     )
-    notice_id = notice_ids[0]
+    notice_id = NOTICE_ID
     notice_repository = NoticeRepository(mongodb_client=fake_mongodb_client)
     notice = notice_repository.get(reference=notice_id)
     pipelines = [notice_normalisation_pipeline, notice_transformation_pipeline, notice_validation_pipeline,

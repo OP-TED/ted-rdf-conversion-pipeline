39947f0d43cf723a90c0531c6ac07ea6780f5a10
Dumitru <d.prijilevschi@gmail.com>
2022-10-11 15:09:43 +0300

    Update notice_process_workflow.py

--- dags/notice_process_workflow.py
+++ dags/notice_process_workflow.py
@@ -86,21 +86,25 @@ def notice_process_workflow():
     selector_branch_before_transformation = BranchPythonOperator(
         task_id=SELECTOR_BRANCH_BEFORE_TRANSFORMATION_TASK_ID,
         python_callable=_selector_branch_before_transformation,
+        trigger_rule=TriggerRule.ALL_DONE,
     )
 
     selector_branch_before_validation = BranchPythonOperator(
         task_id=SELECTOR_BRANCH_BEFORE_VALIDATION_TASK_ID,
         python_callable=_selector_branch_before_validation,
+        trigger_rule=TriggerRule.ALL_DONE,
     )
 
     selector_branch_before_package = BranchPythonOperator(
         task_id=SELECTOR_BRANCH_BEFORE_PACKAGE_TASK_ID,
         python_callable=_selector_branch_before_package,
+        trigger_rule=TriggerRule.ALL_DONE,
     )
 
     selector_branch_before_publish = BranchPythonOperator(
         task_id=SELECTOR_BRANCH_BEFORE_PUBLISH_TASK_ID,
         python_callable=_selector_branch_before_publish,
+        trigger_rule=TriggerRule.ALL_DONE,
     )
 
     stop_processing = PythonOperator(

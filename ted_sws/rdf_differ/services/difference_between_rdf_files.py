e6e53696aa594ad5e889bffeb5ca54f2b3f37535
Mihai Racovit <gratiracovit@gmail.com>
2022-10-13 12:15:26 +0300

    made RDF differ

--- ted_sws/rdf_differ/services/difference_between_rdf_files.py
+++ ted_sws/rdf_differ/services/difference_between_rdf_files.py
@@ -10,20 +10,29 @@ FIRST_RML_FILE = TEST_DATA_PATH / "technical_mapping_F03.rml.ttl"
 SECOND_RML_FILE = TEST_DATA_PATH / "technical_mapping_F06.rml.ttl"
 
 
-def rdf_differ_service(FIRST_RML_FILE: str, SECOND_RML_FILE: str) -> List:
+def rdf_differ_service(FIRST_RML_FILE: str, SECOND_RML_FILE: str):
     """
     Given two RML files representing turtle-encoded RDF,
     check whether they represent the same graph.
     """
-
     first_grath = Graph().parse(FIRST_RML_FILE, format='turtle')
     second_grath = Graph().parse(SECOND_RML_FILE, format='turtle')
     eq = isomorphic(first_grath, second_grath)
     if not eq:
         _, first, second = graph_diff(first_grath, second_grath)
-        print("Present in first:")
-        print(first.serialize(format="nt"))
-        print("Present in second:")
-        print(second.serialize(format="nt"))
-    return eq
+        html_report = f'''
+                <html>
+                    <head>
+                        <title>{'differences between rdf files'}</title>
+                    </head>
+                    <body>
+                        <h1>{{difference in the first file}}</h1>
+                        <p>{first.serialize(format="nt")}</p>
+                        <h2>{{difference in the second file}}</h2>
+                        <p>{second.serialize(format="nt")}</p>
+                    </body>'''
+        rezult = open('differences_between_files.html', 'w')
+        rezult.write(html_report)
+        rezult.close()
+    return rezult
 
--- tests/e2e/rdf_differ/test_difference_between_rdf_files.py
+++ tests/e2e/rdf_differ/test_difference_between_rdf_files.py
@@ -1,9 +1,18 @@
+import codecs
+
+import rdflib
+from rdflib import Graph, RDF
+from rdflib.compare import isomorphic, graph_diff
+from bs4 import BeautifulSoup
 from ted_sws.rdf_differ.services.difference_between_rdf_files import rdf_differ_service
 
 
 def test_rdf_differ_service(first_rml_file, second_rml_file):
-    rdf_differ_service(first_rml_file, second_rml_file)
-    assert first_rml_file != second_rml_file
+    differences_between_files = rdf_differ_service(first_rml_file, second_rml_file)
+    file = open(f"{differences_between_files.name}", "r", encoding='utf-8')
+    assert len(file.read()) > 10
+
+
 
 
 

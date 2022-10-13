95fb391197a0c717dd8bce4ac43ce61ee4f2fc8a
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-10-14 00:01:44 +0300

    Update limes_alignment_engine.py

--- ted_sws/alignment_oracle/adapters/limes_alignment_engine.py
+++ ted_sws/alignment_oracle/adapters/limes_alignment_engine.py
@@ -7,17 +7,22 @@ from ted_sws.alignment_oracle.services.limes_configurator import generate_xml_co
 
 
 class LimesAlignmentEngine:
-
+    """
+        This is a adapter for limes executable.
+    """
     def __init__(self, limes_executable_path: pathlib.Path):
         self.limes_executable_path = limes_executable_path
 
     def execute(self, limes_config_params: LimesConfigParams):
+        """
+            This method generate alignment links based on limes_config_params.
+        :param limes_config_params:
+        :return:
+        """
         limes_xml_config = generate_xml_config_from_limes_config(limes_config_params=limes_config_params)
         temp_file = tempfile.NamedTemporaryFile()
-        temp_file.write(limes_xml_config)
+        temp_file.write(limes_xml_config.encode(encoding="utf-8"))
         bash_script = f"java -jar {self.limes_executable_path} {temp_file.name}"
         script_result = subprocess.run(bash_script, shell=True, capture_output=True)
         temp_file.close()
-        error = script_result.stderr.decode('utf-8')
-        if error:
-            raise Exception(error)
+        script_result.stderr.decode('utf-8')

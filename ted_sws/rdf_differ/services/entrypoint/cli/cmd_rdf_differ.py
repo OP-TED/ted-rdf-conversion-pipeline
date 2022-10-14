373e8a688b8cf9d7556db1120930c53bdf64cd26
Mihai Racovit <gratiracovit@gmail.com>
2022-10-14 09:25:29 +0300

    made RDF differ

---
+++ ted_sws/rdf_differ/services/entrypoint/cli/cmd_rdf_differ.py
@@ -0,0 +1,12 @@
+
+from ted_sws.core.adapters.cmd_runner import CmdRunner as BaseCmdRunner
+
+
+class CmdRunner(BaseCmdRunner):
+
+    def __init__(
+            self,
+
+
+
+    # def run_cmd(self):

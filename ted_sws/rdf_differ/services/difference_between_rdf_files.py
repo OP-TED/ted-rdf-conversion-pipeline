2e7e83c9f3e52b553e42a7361c2a872a7c9ef6ee
Kolea Plesco <kalean.bl@gmail.com>
2022-10-14 10:57:09 +0300

    rdf_differ CLI-tool

--- ted_sws/rdf_differ/services/difference_between_rdf_files.py
+++ ted_sws/rdf_differ/services/difference_between_rdf_files.py
@@ -1,11 +1,9 @@
-from typing import List
-
-import rdflib
 from rdflib import Graph
 from rdflib.compare import isomorphic, graph_diff
+import html
 
 
-def generate_rdf_differ_html_report(first_rml_file: str, second_rml_file: str):
+def generate_rdf_differ_html_report(first_rml_file: str, second_rml_file: str) -> str:
     """
     Given two RML files representing turtle-encoded RDF,
     check whether they represent the same graph.
@@ -13,18 +11,34 @@ def generate_rdf_differ_html_report(first_rml_file: str, second_rml_file: str):
     first_graph = Graph().parse(first_rml_file, format='turtle')
     second_graph = Graph().parse(second_rml_file, format='turtle')
     is_equal_graph = isomorphic(first_graph, second_graph)
-    if not is_equal_graph:
-        _, first, second = graph_diff(first_graph, second_graph)
-        html_differences_report = f'''
-                <html>
+    html_differences_report = f'''
+                <!DOCTYPE html>
+                <html lang="en">
                     <head>
-                        <title>{'differences between rdf files'}</title>
+                        <meta charset="utf-8">
+                        <title>Differences between rdf files</title>
                     </head>
                     <body>
-                        <h1>{{difference in the first file}}</h1>
-                        <p>{first.serialize(format="nt")}</p>
-                        <h2>{{difference in the second file}}</h2>
-                        <p>{second.serialize(format="nt")}</p>
-                    </body>'''
+                        <h1>Differences between rdf files</h1>
+                        <hr>
+                        '''
+    if not is_equal_graph:
+        in_both, in_first, in_second = graph_diff(first_graph, second_graph)
+        html_differences_report += "<h2>Difference in the first file</h2>"
+        html_differences_report += f"<h5>{first_rml_file}</h5>"
+        html_differences_report += "<ul>"
+        for line in in_first.serialize(format='nt', encoding="utf-8").splitlines():
+            if line:
+                html_differences_report += f"<li>{html.escape(line.decode('utf-8'))}</li>"
+        html_differences_report += "</ul>"
+        html_differences_report += "<ul>"
+        html_differences_report += "<h2>Difference in the second file</h2>"
+        html_differences_report += f"<h5>{second_rml_file}</h5>"
+        html_differences_report += "<hr>"
+        for line in in_second.serialize(format='nt', encoding="utf-8").splitlines():
+            if line:
+                html_differences_report += f"<li>{html.escape(line.decode('utf-8'))}</li>"
+        html_differences_report += "</ul>"
+        html_differences_report += "<hr>"
+    html_differences_report += "</body></html>"
     return html_differences_report
-

a62cda1b4da0a4b7c9ef8f5c9bc7c6f027da9590
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2022-10-19 20:55:22 +0300

    Update test_rdf_fragment_processor.py

--- tests/unit/master_data_registry/test_rdf_fragment_processor.py
+++ tests/unit/master_data_registry/test_rdf_fragment_processor.py
@@ -11,8 +11,6 @@ def test_get_rdf_fragment_by_cet_uri_from_string(rdf_content, organisation_cet_u
         assert type(rdf_fragment) == rdflib.Graph
 
 
-
-
 def test_get_rdf_fragments_by_cet_uri_from_file(rdf_file_path, organisation_cet_uri):
     rdf_fragments = get_rdf_fragments_by_cet_uri_from_file(rdf_file_path=rdf_file_path, cet_uri=organisation_cet_uri)
     assert len(rdf_fragments) == 3

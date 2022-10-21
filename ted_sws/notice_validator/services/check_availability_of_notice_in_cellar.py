2c7eb719d9a434afa23314e2f8266f4e29d95423
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-10-21 14:07:37 +0300

    Update check_availability_of_notice_in_cellar.py

--- ted_sws/notice_validator/services/check_availability_of_notice_in_cellar.py
+++ ted_sws/notice_validator/services/check_availability_of_notice_in_cellar.py
@@ -8,9 +8,7 @@ WEBAPI_SPARQL_RUN_FORMAT = "application/sparql-results+json"
 
 def check_availability_of_notice_in_cellar(notice_uri: str, endpoint_url: str = WEBAPI_SPARQL_URL) -> bool:
     query = CELLAR_NOTICE_AVAILABILITY_QUERY.format(notice_uri=notice_uri)
-    result = SPARQLTripleStoreEndpoint(
-        endpoint_url=endpoint_url,
-        use_env_credentials=False).with_query(sparql_query=query).fetch_tree()
+    result = SPARQLTripleStoreEndpoint(endpoint_url=endpoint_url).with_query(sparql_query=query).fetch_tree()
     return result['boolean']
 
 

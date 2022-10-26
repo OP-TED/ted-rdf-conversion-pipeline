6f3f7059990881f4c4981ae437a66ba30fe05502
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-10-26 20:50:52 +0300

    Update test_load_transformed_notice_into_triple_store.py

--- tests/e2e/notice_publisher_triple_store/test_load_transformed_notice_into_triple_store.py
+++ tests/e2e/notice_publisher_triple_store/test_load_transformed_notice_into_triple_store.py
@@ -6,6 +6,7 @@ SPARQL_QUERY_GRAPH = "SELECT ?g {  GRAPH ?g { ?s ?p ?o  } }"
 SPARQL_QUERY_FIXED_URI = "select * { <http://data.europa.eu/a4g/resource/ReviewerOrganisationIdentifier/2018-S-175-396207/de2507f9-ae25-37c8-809c-0109efe10669> ?p ?o .} "
 TMP_FUSEKI_DATASET_NAME = "tmp_dataset_for_tests"
 
+
 def test_load_notice_into_triple_store(transformed_complete_notice, fuseki_triple_store):
     fuseki_triple_store.create_repository(repository_name=TMP_FUSEKI_DATASET_NAME)
     load_rdf_manifestation_into_triple_store(rdf_manifestation=transformed_complete_notice.distilled_rdf_manifestation,
@@ -26,5 +27,5 @@ def test_load_notice_into_triple_store(transformed_complete_notice, fuseki_tripl
     df_query_result = sparql_endpoint.with_query(sparql_query=SPARQL_QUERY_FIXED_URI).fetch_tabular()
     assert df_query_result is not None
     assert len(df_query_result) > 0
-    
+
     fuseki_triple_store.delete_repository(repository_name=TMP_FUSEKI_DATASET_NAME)

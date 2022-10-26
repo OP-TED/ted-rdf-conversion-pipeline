e53db774e77425983cbf69d44cc1f1486e21b248
Mihai Racovit <gratiracovit@gmail.com>
2022-10-26 20:28:02 +0300

    updated load transformed notice

--- ted_sws/notice_publisher_triple_store/services/load_transformed_notice_into_triple_store.py
+++ ted_sws/notice_publisher_triple_store/services/load_transformed_notice_into_triple_store.py
@@ -2,13 +2,13 @@
     This module implements functionality to load a given notice into a triple store.
 """
 from ted_sws.data_manager.adapters.repository_abc import NoticeRepositoryABC
-from ted_sws.data_manager.adapters.triple_store import TripleStoreEndpointABC, RDF_MIME_TYPES
+from ted_sws.data_manager.adapters.triple_store import TripleStoreABC, RDF_MIME_TYPES
 
 DEFAULT_NOTICE_REPOSITORY_NAME = "notices"
 
 
 def load_notice_into_triple_store(notice_id: str, notice_repository: NoticeRepositoryABC,
-                                  triple_store_repository: TripleStoreEndpointABC,
+                                  triple_store_repository: TripleStoreABC,
                                   repository_name: str = DEFAULT_NOTICE_REPOSITORY_NAME):
     """
 
@@ -18,4 +18,4 @@ def load_notice_into_triple_store(notice_id: str, notice_repository: NoticeRepos
         raise ValueError('Notice, with "%s" notice_id, was not found' % notice_id)
     mime_type = RDF_MIME_TYPES
     rdf_manifestation_string = notice.rdf_manifestation.object_data
-    triple_store_repository.add_data_to_repository(file_content=rdf_manifestation_string, repository_name=repository_name, mime_type=mime_type)
+    triple_store_repository.add_data_to_repository(file_content=rdf_manifestation_string.encode(encoding='utf-8'), repository_name=repository_name, mime_type=mime_type)

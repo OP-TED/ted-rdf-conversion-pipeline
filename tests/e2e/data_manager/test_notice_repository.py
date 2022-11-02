c589642fe92cfd24f23309568c60923268139163
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-11-02 23:36:42 +0200

    Update test_notice_repository.py

--- tests/e2e/data_manager/test_notice_repository.py
+++ tests/e2e/data_manager/test_notice_repository.py
@@ -12,23 +12,23 @@ TEST_DATABASE_NAME = "test_database_name"
 def test_notice_repository_create(mongodb_client):
     mongodb_client.drop_database(TEST_DATABASE_NAME)
     notice_repository = NoticeRepository(mongodb_client=mongodb_client, database_name=TEST_DATABASE_NAME)
-    notice = Notice(ted_id=NOTICE_TED_ID, original_metadata=TEDMetadata(**{"AA": "Metadata"}),
+    notice = Notice(ted_id=NOTICE_TED_ID, original_metadata=TEDMetadata(**{"AA": ["Metadata"]}),
                     xml_manifestation=XMLManifestation(object_data="HELLO"))
     notice_repository.add(notice)
     result_notice = notice_repository.get(reference=NOTICE_TED_ID)
     assert result_notice
     assert result_notice.ted_id == NOTICE_TED_ID
-    assert result_notice.original_metadata.AA == "Metadata"
+    assert result_notice.original_metadata.AA == ["Metadata"]
     result_notices = list(notice_repository.list())
     assert result_notices
     assert len(result_notices) == 1
     notice_repository.add(notice)
-    notice.original_metadata = TEDMetadata(**{"AA": "Updated metadata"})
+    notice.original_metadata = TEDMetadata(**{"AA": ["Updated metadata"]})
     notice_repository.update(notice)
     result_notice = notice_repository.get(reference=NOTICE_TED_ID)
     assert result_notice
     assert result_notice.ted_id == NOTICE_TED_ID
-    assert result_notice.original_metadata.AA == "Updated metadata"
+    assert result_notice.original_metadata.AA == ["Updated metadata"]
     mongodb_client.drop_database(TEST_DATABASE_NAME)
 
 

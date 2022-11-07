f979e0aa4e1810ea2bdc0176136430b77fdb5898
Dumitru <d.prijilevschi@gmail.com>
2022-11-07 18:02:31 +0300

    delete unnecessary logs

--- dags/daily_materialized_view_update.py
+++ dags/daily_materialized_view_update.py
@@ -5,8 +5,6 @@ from dags import DEFAULT_DAG_ARGUMENTS
 from ted_sws import config
 from ted_sws.data_manager.services.create_batch_collection_materialised_view import \
     create_batch_collection_materialised_view
-from ted_sws.data_manager.services.create_dag_collection_materialised_view import \
-    create_dag_collection_materialised_view
 from ted_sws.data_manager.services.create_notice_collection_materialised_view import \
     create_notice_collection_materialised_view, update_notice_collection_materialised_view
 
@@ -29,12 +27,7 @@ def daily_materialized_view_update():
         update_notice_collection_materialised_view(mongo_client=mongo_client)
         create_batch_collection_materialised_view(mongo_client=mongo_client)
 
-    @task
-    def create_dag_materialised_view():
-        mongo_client = MongoClient(config.MONGO_DB_AUTH_URL)
-        create_dag_collection_materialised_view(mongo_client=mongo_client)
-
-    create_materialised_view() >> update_materialised_view_with_logs() >> create_dag_materialised_view()
+    create_materialised_view() >> update_materialised_view_with_logs()
 
 
 dag = daily_materialized_view_update()

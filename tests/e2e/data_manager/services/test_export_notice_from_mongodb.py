bc324e7d366c6720f8bc8f5c4ccee6e21dba9033
Kolea Plesco <kalean.bl@gmail.com>
2022-11-16 08:47:34 +0200

    Export notice tests

--- tests/e2e/data_manager/services/test_export_notice_from_mongodb.py
+++ tests/e2e/data_manager/services/test_export_notice_from_mongodb.py
@@ -1,14 +1,20 @@
 import tempfile
+from pathlib import Path
 
 from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
 from ted_sws.data_manager.services.export_notice_from_mongodb import export_notice_by_id
 
 
-def test_export_notice_by_id(transformed_complete_notice, fake_mongodb_client):
+def test_export_notice_by_id(packaged_notice, fake_mongodb_client):
     notice_repository = NoticeRepository(mongodb_client=fake_mongodb_client)
-    notice_repository.add(transformed_complete_notice)
+    notice_repository.add(packaged_notice)
     with tempfile.TemporaryDirectory() as tmp_dirname:
-        is_saved, saved_path = export_notice_by_id(notice_id=transformed_complete_notice.ted_id,
+        is_saved, saved_path = export_notice_by_id(notice_id=packaged_notice.ted_id,
                                                    output_folder=tmp_dirname,
                                                    mongodb_client=fake_mongodb_client)
         assert is_saved
+        assert saved_path == str(Path(tmp_dirname) / packaged_notice.ted_id)
+
+    is_saved = export_notice_by_id(notice_id="invalid_notice_id",
+                                   output_folder=tmp_dirname)
+    assert not is_saved

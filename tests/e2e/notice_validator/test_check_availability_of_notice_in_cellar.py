c0815ed90cc49923669bb4d5a6fb9eb213510ac4
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2022-11-18 13:31:26 +0200

    WIP

--- tests/e2e/notice_validator/test_check_availability_of_notice_in_cellar.py
+++ tests/e2e/notice_validator/test_check_availability_of_notice_in_cellar.py
@@ -1,10 +1,6 @@
-import pytest
-
 from ted_sws.core.model.notice import NoticeStatus
-from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
 from ted_sws.notice_validator.services.check_availability_of_notice_in_cellar import \
-    check_availability_of_notice_in_cellar, validate_notice_availability_in_cellar, \
-    validate_and_update_notice_availability_in_cellar_by_id, validate_and_update_notice_availability_in_cellar
+    check_availability_of_notice_in_cellar, validate_notice_availability_in_cellar
 
 
 def test_check_availability_of_notice_in_cellar(valid_cellar_uri, invalid_cellar_uri):
@@ -23,42 +19,3 @@ def test_validate_notice_availability_in_cellar(fake_notice_F03, valid_cellar_ur
     fake_notice_F03._status = NoticeStatus.PUBLISHED
     validate_notice_availability_in_cellar(notice=fake_notice_F03, notice_uri=invalid_cellar_uri)
     assert fake_notice_F03.status == NoticeStatus.PUBLICLY_UNAVAILABLE
-
-
-def test_validate_and_update_notice_availability_in_cellar_by_id(fake_mongodb_client, fake_notice_F03,
-                                                                 valid_cellar_uri):
-    fake_notice_F03._status = NoticeStatus.PUBLISHED
-
-    notice_repository: NoticeRepository = NoticeRepository(mongodb_client=fake_mongodb_client)
-    notice_repository.add(fake_notice_F03)
-
-    notice = validate_and_update_notice_availability_in_cellar_by_id(notice_id=fake_notice_F03.ted_id,
-                                                                     mongodb_client=fake_mongodb_client,
-                                                                     notice_uri=valid_cellar_uri)
-    assert notice.status == NoticeStatus.PUBLICLY_AVAILABLE
-
-    fake_notice_F03._status = NoticeStatus.PUBLISHED
-    with pytest.raises(ValueError):
-        validate_and_update_notice_availability_in_cellar_by_id(notice_id="invalid_notice_id",
-                                                                mongodb_client=fake_mongodb_client,
-                                                                notice_uri=valid_cellar_uri)
-        assert notice.status == NoticeStatus.PUBLISHED
-
-
-def test_validate_and_update_notice_availability_in_cellar(fake_mongodb_client, fake_notice_F03,
-                                                           valid_cellar_uri):
-    fake_notice_F03._status = NoticeStatus.PUBLISHED
-
-    notice_repository: NoticeRepository = NoticeRepository(mongodb_client=fake_mongodb_client)
-    notice_repository.add(fake_notice_F03)
-
-    db_notice_with_old_status = notice_repository.get(reference=fake_notice_F03.ted_id)
-    assert db_notice_with_old_status.status == NoticeStatus.PUBLISHED
-
-    notice = validate_and_update_notice_availability_in_cellar(notice=fake_notice_F03,
-                                                               mongodb_client=fake_mongodb_client,
-                                                               notice_uri=valid_cellar_uri)
-    assert notice.status == NoticeStatus.PUBLICLY_AVAILABLE
-
-    db_notice_with_new_status = notice_repository.get(reference=fake_notice_F03.ted_id)
-    assert db_notice_with_new_status.status == NoticeStatus.PUBLICLY_AVAILABLE
--- tests/e2e/supra_notice_manager/test_supra_notice_validator.py
+++ tests/e2e/supra_notice_manager/test_supra_notice_validator.py
@@ -3,12 +3,11 @@ from datetime import datetime, time, timedelta
 import pytest
 
 from ted_sws.core.model.manifestation import XMLManifestation, ValidationSummaryReport
-from ted_sws.core.model.notice import Notice, NoticeStatus
-from ted_sws.data_manager.adapters.notice_repository import NoticeRepository
+from ted_sws.core.model.notice import Notice
 from ted_sws.supra_notice_manager.services.daily_supra_notice_manager import \
     create_and_store_in_mongo_db_daily_supra_notice
 from ted_sws.supra_notice_manager.services.supra_notice_validator import validate_and_update_daily_supra_notice, \
-    validate_and_update_supra_notice_availability_in_cellar, summary_validation_for_daily_supra_notice
+    summary_validation_for_daily_supra_notice
 
 
 def test_supra_notice_validator(fake_mongodb_client, daily_supra_notice_repository):
@@ -54,31 +53,3 @@ def test_summary_validation_for_daily_supra_notice(fake_mongodb_client, daily_su
     assert result.validation_summary.notice_ids == []
 
 
-def test_validate_and_update_supra_notice_availability_in_cellar(fake_mongodb_client, daily_supra_notice_repository,
-                                                                 fake_notice_repository):
-    day = datetime.combine(datetime.today() - timedelta(days=1), time())
-
-    with pytest.raises(ValueError):
-        validate_and_update_supra_notice_availability_in_cellar(day, fake_mongodb_client)
-
-    notice_id = "TEST-XYZ067623-2022023"
-    notice_ids = [notice_id]
-
-    notice = Notice(ted_id=notice_id, xml_manifestation=XMLManifestation(object_data=""))
-    notice._status = NoticeStatus.PUBLISHED
-
-    fake_notice_repository.add(notice)
-
-    create_and_store_in_mongo_db_daily_supra_notice(notice_ids=notice_ids, mongodb_client=fake_mongodb_client,
-                                                    notice_fetched_date=day)
-    validate_and_update_supra_notice_availability_in_cellar(day, fake_mongodb_client)
-    result = daily_supra_notice_repository.get(reference=day)
-
-    assert result
-    # FIXME: Temporally disable DailySupraNotice validation not_published_notice_ids update
-    # assert len(result.validation_report.not_published_notice_ids) > 0
-    # assert notice_id in result.validation_report.not_published_notice_ids
-    # assert not result.validation_report.is_valid()
-
-    notice = fake_notice_repository.get(reference=notice_id)
-    assert notice.status == NoticeStatus.PUBLICLY_UNAVAILABLE

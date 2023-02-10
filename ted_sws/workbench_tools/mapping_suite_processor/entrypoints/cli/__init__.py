6ae44936a4db285887d88a47679b28aa1192fb83
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2023-02-10 17:36:50 +0200

    migrate work_bench component from ted_sws

--- ted_sws/workbench_tools/mapping_suite_processor/entrypoints/cli/__init__.py
+++
@@ -1,6 +0,0 @@
-from ted_sws.data_manager.adapters.mapping_suite_repository import MS_TRANSFORM_FOLDER_NAME, \
-    MS_CONCEPTUAL_MAPPING_FILE_NAME
-
-CONCEPTUAL_MAPPINGS_FILE_TEMPLATE = '{mappings_path}/{mapping_suite_id}/' + MS_TRANSFORM_FOLDER_NAME + '/' \
-                                    + MS_CONCEPTUAL_MAPPING_FILE_NAME
-MAPPING_SUITE_FILE_TEMPLATE = '{mappings_path}/{mapping_suite_id}'
--- ted_sws/workbench_tools/notice_validator/cli/__init__.py
+++
@@ -1,4 +0,0 @@
-from ted_sws.core.adapters.cmd_runner import DEFAULT_OUTPUT_PATH
-
-DEFAULT_RDF_FOLDER = '{mappings_path}/{mapping_suite_id}/' + DEFAULT_OUTPUT_PATH
-DEFAULT_TEST_SUITE_REPORT_FOLDER = "test_suite_report"

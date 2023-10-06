dd053edb71e1c93e34416ace328610eade3615d6
Dumitru <d.prijilevschi@gmail.com>
2023-10-06 16:02:45 +0300

    Update daily_notices_metadata_update.py

--- dags/daily_notices_metadata_update.py
+++ dags/daily_notices_metadata_update.py
@@ -47,8 +47,8 @@ END_DATE_PARAM_KEY = "end_date"
 def daily_notices_metadata_update():
     @task
     def update_daily_notices_metadata_from_ted_api():
-        start_date = get_dag_param(key=START_DATE_PARAM_KEY, raise_error=True)
-        end_date = get_dag_param(key=END_DATE_PARAM_KEY, raise_error=True)
+        start_date = get_dag_param(key=START_DATE_PARAM_KEY, raise_error=False)
+        end_date = get_dag_param(key=END_DATE_PARAM_KEY, raise_error=False)
 
         update_daily_notices_metadata_from_ted(start_date=datetime.strptime(start_date, "%Y-%m-%d"),
                                                end_date=datetime.strptime(end_date, "%Y-%m-%d"))

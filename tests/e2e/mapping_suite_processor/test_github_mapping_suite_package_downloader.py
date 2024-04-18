1e581c05b2b5e706b91d9d2b2e4f50c92a4a0c7a
Dragos0000 <dragos.paun0000@gmail.com>
2024-04-18 12:57:52 +0100

    removal of GITHUB_TED_SWS_ARTEFACTS_REPOSITORY_NAME variable

--- tests/e2e/mapping_suite_processor/test_github_mapping_suite_package_downloader.py
+++ tests/e2e/mapping_suite_processor/test_github_mapping_suite_package_downloader.py
@@ -1,6 +1,7 @@
 import pathlib
 from ted_sws import config
-from ted_sws.mapping_suite_processor.adapters.github_package_downloader import GitHubMappingSuitePackageDownloader
+from ted_sws.mapping_suite_processor.adapters.github_package_downloader import GitHubMappingSuitePackageDownloader, \
+    get_repo_name_from_repo_url
 from tests.e2e.mapping_suite_processor import MAPPING_SUITE_PACKAGE_NAME
 
 
@@ -11,3 +12,10 @@ def test_github_mapping_suite_package_downloader(tmpdir):
     mapping_suite_package_downloader.download(output_mapping_suite_package_path=tmp_dir_path)
     mapping_suite_package_path = tmp_dir_path / MAPPING_SUITE_PACKAGE_NAME
     assert mapping_suite_package_path.is_dir()
+
+
+def test_get_repo_name_from_repo_url():
+    repo_url = "https://github.com/OP-TED/ted-rdf-mapping.git"
+    repo_name = get_repo_name_from_repo_url(repository_url=repo_url)
+    assert repo_name == "ted-rdf-mapping"
+    assert isinstance(repo_name, str)

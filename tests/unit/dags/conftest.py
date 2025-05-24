7d20fdfb466ce51f6299fb13f7670ec2c2a8407a
duprijil <d.prijilevschi@gmail.com>
2025-05-24 11:23:38 +0300

    Update conftest.py

--- tests/unit/dags/conftest.py
+++ tests/unit/dags/conftest.py
@@ -5,16 +5,32 @@ import pytest
 from airflow.models import DagBag, Variable
 from airflow.timetables.trigger import CronTriggerTimetable
 from airflow.utils.db import resetdb, initdb
+
 from dags.daily_materialized_views_update import DAILY_MATERIALISED_VIEWS_DAG_NAME
 from dags.fetch_notices_by_date import FETCHER_DAG_NAME
 from tests import AIRFLOW_DAG_FOLDER
 
-@pytest.fixture(scope="function")
-def setup_airflow():
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_airflow_for_all_tests():
+    """Setup Airflow DB once at the beginning of the test session"""
     temp_db_file = tempfile.NamedTemporaryFile(mode="w+", suffix=".db")
     os.environ["AIRFLOW__CORE__SQL_ALCHEMY_CONN"] = f"sqlite:///{temp_db_file.name}"
     os.environ["AIRFLOW__CORE__LOAD_EXAMPLES"] = "False"
     os.environ["AIRFLOW__CORE__UNIT_TEST_MODE"] = "True"
+    resetdb()  # Reset the database first to ensure clean state
+    initdb()
+    yield
+    # Cleanup after all tests complete
+    if os.path.exists(temp_db_file.name):
+        temp_db_file.close()
+
+
+@pytest.fixture(scope="function")
+def setup_airflow():
+    """This fixture now just makes sure the DB is clean for each test,
+    but doesn't reinitialize the connection"""
+    resetdb()
     initdb()
 
 
@@ -26,7 +42,7 @@ def dag_bag(dag_materialised_view_update_schedule_variable_name,
     Variable.delete(key=dag_fetch_schedule_variable_name)
     return DagBag(
         dag_folder=AIRFLOW_DAG_FOLDER,
-        include_examples=False, #Same as: os.environ["AIRFLOW__CORE__LOAD_EXAMPLES"] = "False"
+        include_examples=False,  # Same as: os.environ["AIRFLOW__CORE__LOAD_EXAMPLES"] = "False"
         read_dags_from_db=False,
         collect_dags=True)
 

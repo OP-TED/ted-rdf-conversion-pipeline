dec7e7dd09ccaedb13e003076c2b6dbb72336ee8
duprijil <d.prijilevschi@gmail.com>
2025-05-28 01:27:46 +0300

    fix: Fix problem with running unnecessary scheduled dag run

--- dags/__init__.py
+++ dags/__init__.py
@@ -5,7 +5,7 @@ from airflow import Dataset
 DEFAULT_DAG_ARGUMENTS = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime.today(),
+    "start_date": datetime(2022, 1, 1),
     "email": ["info@meaningfy.ws"],
     "email_on_failure": False,
     "email_on_retry": False,
--- dags/fetch_notices_by_date.py
+++ dags/fetch_notices_by_date.py
@@ -36,6 +36,7 @@ VALIDATE_FETCHED_NOTICES_TASK_ID = "validate_fetched_notices"
      schedule=CronTriggerTimetable(
          cron=config.SCHEDULE_DAG_FETCH,
          timezone=DAG_DEFAULT_TIMEZONE),
+     start_date=datetime.today(),
      tags=['selector', 'daily-fetch'],
      params={
          WILD_CARD_DAG_KEY: Param(

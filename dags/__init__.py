fa46d7edccf4c9fae6ab00c90a6bfc4e3b259b06
duprijil <d.prijilevschi@gmail.com>
2025-05-28 01:11:11 +0300

    fix: Fix problem with running unnecessary scheduled dag run on DAG unpause

--- dags/__init__.py
+++ dags/__init__.py
@@ -5,7 +5,7 @@ from airflow import Dataset
 DEFAULT_DAG_ARGUMENTS = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2022, 1, 1),
+    "start_date": datetime.today(),
     "email": ["info@meaningfy.ws"],
     "email_on_failure": False,
     "email_on_retry": False,

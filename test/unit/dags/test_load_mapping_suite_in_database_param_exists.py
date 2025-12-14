7955a18ae0f55998d1eea6fbcb8a89b18b666949
duprijil <d.prijilevschi@gmail.com>
2025-12-14 13:40:35 +0200

    fix: Add materialised view dag run toggle in load mapping suite dag

---
+++ test/unit/dags/test_load_mapping_suite_in_database_param_exists.py
@@ -0,0 +1,10 @@
+from airflow.models import DagBag
+
+from src.dags import RUN_MATERIALISED_VIEW_DAG_PARAM
+from src.dags.load_mapping_suite_in_database import DAG_ID as DAG_NAME
+
+
+def test_run_materialised_view_param_exists_in_load_mapping_suite_in_database(dag_bag: DagBag):
+    assert DAG_NAME in dag_bag.dags
+    dag = dag_bag.dags[DAG_NAME]
+    assert RUN_MATERIALISED_VIEW_DAG_PARAM in dag.params
